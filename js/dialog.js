e2cf97bc697b4fb1f91cb8a0cf794ccb49b1bbcc
Stoodkev <quentin.correa@gmail.com>
2018-07-19 17:03:28 +0800

    Dialog for errors

--- js/dialog.js
+++ js/dialog.js
@@ -1,7 +1,12 @@
-console.log("js load");
 chrome.runtime.onMessage.addListener(function(msg,sender,sendResp){
-  console.log("received");
   if(msg.command=="sendDialogInfo"){
-    $("#dialog_header").html(msg.request);
+    if(!msg.msg.success){
+      $("#dialog_header").html("Error");
+      $("#dialog_header").addClass("error_header");
+      $("#error_dialog").html(msg.msg.message);
+      $(".modal-body button").click(function(){
+          window.close();
+      });
+    }
   }
 });
