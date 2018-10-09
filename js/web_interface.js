0594eb237e913a4124ae51043c43439ca185b475
Matt Rosen <matt@mrosen.com>
2018-10-08 20:06:17 -0400

    Fix for requesting custom json transaction

--- js/web_interface.js
+++ js/web_interface.js
@@ -53,11 +53,13 @@ function sendResponse(response) {
 }
 
 function validate(req) {
-    return req != null && req != undefined && req.type != undefined && req.type != null && ((req.type == "decode" && isFilled(req.username) && isFilled(req.message) && req.message[0] == "#" && isFilledKey(req.method)) ||
-        (req.type == "vote" && isFilled(req.username) && isFilledWeight(req.weight) && isFilled(req.permlink) && isFilled(req.author)) ||
-        (req.type == "post" && isFilled(req.username) && isFilled(req.title) && isFilled(req.body) && isFilled(req.permlink) && isFilled(req.parent_perm) && isFilled(req.json_metadata) && isCustomOptions(req))||
-        (req.type == "delegation" && isFilled(req.username) && isFilled(req.delegatee) && isFilledAmtSP(req.sp)) ||
-        (req.type == "transfer" && isFilledAmt(req.amount) && isFilled(req.to) && isFilledCurrency(req.currency)));
+		return req != null && req != undefined && req.type != undefined && req.type != null && 
+			((req.type == "decode" && isFilled(req.username) && isFilled(req.message) && req.message[0] == "#" && isFilledKey(req.method)) ||
+       (req.type == "vote" && isFilled(req.username) && isFilledWeight(req.weight) && isFilled(req.permlink) && isFilled(req.author)) ||
+			 (req.type == "post" && isFilled(req.username) && isFilled(req.title) && isFilled(req.body) && isFilled(req.permlink) && isFilled(req.parent_perm) && isFilled(req.json_metadata) && isCustomOptions(req))||
+			 (req.type == "custom" && isFilled(req.username) && isFilled(req.json) && isFilled(req.id)) ||
+       (req.type == "delegation" && isFilled(req.username) && isFilled(req.delegatee) && isFilledAmtSP(req.sp)) ||
+       (req.type == "transfer" && isFilledAmt(req.amount) && isFilled(req.to) && isFilledCurrency(req.currency)));
 }
 
 
