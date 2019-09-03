f3b6ece97b9d247676dbed7dffc99afaf9c03897
Stoodkev <quentin.correa@gmail.com>
2019-09-03 10:36:16 +0800

    Extract RPCs list

---
+++ js/rpcs.js
@@ -0,0 +1,11 @@
+const RPCs = [
+  "https://api.steemit.com",
+  "https://anyx.io",
+  "https://rpc.usesteem.com",
+  "https://api.steemitdev.com",
+  "https://api.steem.house",
+  "https://steemd.minnowsupportproject.org",
+  "https://steemd.privex.io",
+  "https://appbasetest.timcliff.com",
+  "TESTNET"
+];
