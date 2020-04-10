522a9559e9e663c9985a0eaab4cc08890f0f691f
Stoodkev <quentin.correa@gmail.com>
2020-04-10 16:37:32 +0800

    add error handling

--- js/background/ops/broadcast.js
+++ js/background/ops/broadcast.js
@@ -6,11 +6,11 @@ const broadcastData = data => {
     let account = accountsList.get(data.username);
 
     // check if operations contains any transfer wich requires memo encryption
-    operations.map(async op => {
-      if (op[0] == "transfer") {
-        const memo = op[1].memo;
-        if (memo && memo.length > 0 && memo[0] == "#") {
-          try {
+    try {
+      operations.map(async op => {
+        if (op[0] == "transfer") {
+          const memo = op[1].memo;
+          if (memo && memo.length > 0 && memo[0] == "#") {
             const receiver = await steem.api.getAccountsAsync([op[1].to]);
             console.log("receiver", receiver);
             const memoReceiver = receiver[0].memo_key;
@@ -22,33 +22,40 @@ const broadcastData = data => {
             );
             console.log(op[1].memo);
             return op;
-          } catch (e) {
-            console.log(e);
           }
         }
-      }
-    });
-    console.log(operations);
+      });
+      console.log(operations);
 
-    steem.broadcast.send(
-      {
-        operations: operations,
-        extensions: []
-      },
-      broadcastKeys,
-      (err, result) => {
-        console.log(result, err);
-        const err_message = beautifyErrorMessage(err);
+      steem.broadcast.send(
+        {
+          operations: operations,
+          extensions: []
+        },
+        broadcastKeys,
+        (err, result) => {
+          console.log(result, err);
+          const err_message = beautifyErrorMessage(err);
 
-        const message = createMessage(
-          err,
-          result,
-          data,
-          chrome.i18n.getMessage("bgd_ops_broadcast"),
-          err_message
-        );
-        resolve(message);
-      }
-    );
+          const message = createMessage(
+            err,
+            result,
+            data,
+            chrome.i18n.getMessage("bgd_ops_broadcast"),
+            err_message
+          );
+          resolve(message);
+        }
+      );
+    } catch (e) {
+      const message = createMessage(
+        true,
+        null,
+        data,
+        null,
+        "Could not encode transfer."
+      );
+      resolve(message);
+    }
   });
 };
