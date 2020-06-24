ce8f70b6c671a6e86556d146a791edba7a677dd6
Stoodkev <quentin.correa@gmail.com>
2020-06-24 23:35:50 +0800

    add exchanges info

--- js/libs/renderExchanges.js
+++ js/libs/renderExchanges.js
@@ -1,26 +1,42 @@
 const exchanges = [
   {
-    account: "bittrex",
+    name: "bittrex",
     tokens: ["HIVE", "HBD"],
     url_hive: "https://global.bittrex.com/Market/Index?MarketName=BTC-HIVE",
     url_hbd: "https://global.bittrex.com/Market/Index?MarketName=BTC-HBD"
   },
-  { account: "deepcrypto8", tokens: ["HIVE"], url_hive: "" },
   {
-    account: "ionomy",
+    name: "binance",
+    tokens: ["HIVE"],
+    url_hive: "https://www.binance.com/en/trade/HIVE_BTC"
+  },
+  {
+    name: "ionomy",
     tokens: ["HIVE", "HBD"],
     url_hive: "https://ionomy.com/en/markets/btc-hive",
     url_hbd: "https://ionomy.com/en/markets/btc-hbd"
   },
-  { account: "huobi-pro", tokens: ["HIVE"], url_hive: "" },
-  { account: "mxchive", tokens: ["HIVE"], url_hive: "" },
-  { account: "probithive", tokens: ["HIVE"], url_hive: "" }
+  {
+    name: "huobi",
+    tokens: ["HIVE"],
+    url_hive: "https://www.huobi.com/en-us/exchange/hive_usdt/"
+  },
+  {
+    name: "mxc",
+    tokens: ["HIVE"],
+    url_hive: "https://www.mxc.com/trade/easy#HIVE_USDT"
+  },
+  {
+    name: "probit",
+    tokens: ["HIVE"],
+    url_hive: "https://www.probit.com/app/exchange/HIVE-USDT"
+  }
 ];
 
 const renderExchangesList = crypto => {
   let exchangesList = exchanges.filter(e => e.tokens.includes(crypto));
   console.log(crypto, exchangesList);
-  const COLS_PER_ROW = 3;
+  const COLS_PER_ROW = 2;
   const rows = Math.ceil(exchangesList.length / COLS_PER_ROW);
   console.log(rows);
   let html = "";
@@ -43,7 +59,7 @@ const renderExchange = (exchange, crypto) => {
   return `
   <a href="${exchange[`url_${crypto}`]}" target="_blank">
   <div class="img_wrapper"><div>
-  <img src="../images/${exchange.account}.png"/>
+  <img src="../images/${exchange.name}.png"/>
   </div></div>
   </a>
   `;
