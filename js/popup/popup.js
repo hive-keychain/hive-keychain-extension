e9564a6fd2150abb10aa43132240e2c15f4b7e15
stoodkev <quentin.correa@gmail.com>
2020-12-16 15:08:56 +0800

    fix laggy ui on secondary screen (macOS)

--- js/popup/popup.js
+++ js/popup/popup.js
@@ -11,6 +11,43 @@ const transferValidator = new TransferValidator();
 $("#copied").hide();
 $("#witness_votes").hide();
 
+/**
+ * Temporary workaround for secondary monitors on MacOS where redraws don't happen
+ * @See https://bugs.chromium.org/p/chromium/issues/detail?id=971701
+ */
+if (
+  // From testing the following conditions seem to indicate that the popup was opened on a secondary monitor
+  window.screenLeft < 0 ||
+  window.screenTop < 0 ||
+  window.screenLeft > window.screen.width ||
+  window.screenTop > window.screen.height
+) {
+  chrome.runtime.getPlatformInfo(function(info) {
+    if (info.os === "mac") {
+      const fontFaceSheet = new CSSStyleSheet();
+      fontFaceSheet.insertRule(`
+        @keyframes redraw {
+          0% {
+            opacity: 1;
+          }
+          100% {
+            opacity: .99;
+          }
+        }
+      `);
+      fontFaceSheet.insertRule(`
+        html {
+          animation: redraw 1s linear infinite;
+        }
+      `);
+      document.adoptedStyleSheets = [
+        ...document.adoptedStyleSheets,
+        fontFaceSheet
+      ];
+    }
+  });
+}
+
 // Ask background if it is unlocked
 getMK();
 
