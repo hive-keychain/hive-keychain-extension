e8fe96436bf76c77e5a3b57f647bc95e5f314836
stoodkev <quentin.correa@gmail.com>
2021-05-31 15:32:22 +0800

    scaffold refactor

--- js/background/dialog_lifecycle.js
+++ js/background/dialog_lifecycle.js
@@ -1,4 +1,4 @@
-const createPopup = (callback, popupHtml = "html/dialog.html") => {
+const createPopup = (callback, popupHtml = "public/dialog.html") => {
   let width = 350;
   confirmed = false;
   //Ensuring only one window is opened by the extension at a time.
@@ -7,7 +7,7 @@ const createPopup = (callback, popupHtml = "html/dialog.html") => {
     id_win = null;
   }
   //Create new window on the top right of the screen
-  chrome.windows.getCurrent(w => {
+  chrome.windows.getCurrent((w) => {
     console.log(popupHtml);
     chrome.windows.create(
       {
@@ -16,16 +16,16 @@ const createPopup = (callback, popupHtml = "html/dialog.html") => {
         height: 566,
         width: width,
         left: w.width - width + w.left,
-        top: w.top
+        top: w.top,
       },
-      win => {
+      (win) => {
         id_win = win.id;
         // Window create fails to take into account window size so it s updated afterwhile.
         chrome.windows.update(win.id, {
           height: 566,
           width: width,
           top: w.top,
-          left: w.width - width + w.left
+          left: w.width - width + w.left,
         });
 
         if (typeof callback === "function") {
@@ -40,7 +40,7 @@ const createPopup = (callback, popupHtml = "html/dialog.html") => {
   });
 };
 
-chrome.windows.onRemoved.addListener(id => {
+chrome.windows.onRemoved.addListener((id) => {
   if (id == id_win && !confirmed) {
     console.log("error6");
     chrome.tabs.sendMessage(tab, {
@@ -51,17 +51,17 @@ chrome.windows.onRemoved.addListener(id => {
         result: null,
         data: request,
         message: chrome.i18n.getMessage("bgd_lifecycle_request_canceled"),
-        request_id: request_id
-      }
+        request_id: request_id,
+      },
     });
   }
 });
 
 // check if win exists before removing it
-const removeWindow = id_win => {
+const removeWindow = (id_win) => {
   console.log(id_win);
-  chrome.windows.getAll(windows => {
-    const hasWin = windows.filter(win => {
+  chrome.windows.getAll((windows) => {
+    const hasWin = windows.filter((win) => {
       return win.id == id_win;
     }).length;
     if (hasWin) {
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: { popup: "./src/popup/index.js", dialog: "./src/dialog/index.js" },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "[name]Bundle.js",
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+    hotOnly: true,
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+};
