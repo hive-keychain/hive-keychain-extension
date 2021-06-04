fb3ba36b623d1a3f168bd9465cba9a977bcfd8f9
stoodkev <quentin.correa@gmail.com>
2021-06-04 17:15:33 +0800

    update refactor scaffolding

--- src/popup/App.js
+++ src/popup/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>{chrome.i18n.getMessage("popup_html_new_password")}</h1>
+        <h1>Hello worldgrd</h1>
       </div>
     );
   }
