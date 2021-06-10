b63cf8aefa15a681c395c3b9d10948e19b837868
Cedric Guillas <cedric.guillas78@gmail.com>
2021-06-10 20:02:03 +0800

    feat(popup): Added default size

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -2,16 +2,17 @@ import { setMsg } from "@popup/actions";
 import { RootState } from "@popup/store";
 import React, { useEffect } from "react";
 import { connect, ConnectedProps } from "react-redux";
+import "./App.css";
 
 const App = ({ setMsg, msg }: PropsFromRedux) => {
   // just for testing action/reducer, you can delete
   useEffect(() => {
-    setMsg("yo");
+    setMsg("hello");
   }, [setMsg]);
 
   return (
     <div className="App">
-      <h1>{msg}</h1>
+      <div>{msg}</div>
     </div>
   );
 };
