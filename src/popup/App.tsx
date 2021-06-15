af3f2a3e1ec48887240133ccd9040969d09aaaf4
stoodkev <quentin.correa@gmail.com>
2021-06-15 17:11:41 +0800

    fix mac freeze issue on external screen

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,56 +1,93 @@
+import { getAccounts } from "@popup/actions/account.actions";
+import { setMk } from "@popup/actions/mk.actions";
+import { navigateTo } from "@popup/actions/navigation.actions";
 import { RootState } from "@popup/store";
 import React, { useEffect } from "react";
 import { connect, ConnectedProps } from "react-redux";
-import "./App.css";
-import { getAccounts } from '@popup/actions/account.actions';
-import { navigateTo } from '@popup/actions/navigation.actions';
-import { SignInComponent } from "./pages/sign-in/sign-in.component";
-import { SignUpComponent } from "./pages/sign-up/sign-up.component";
-import { BackgroundCommand } from "src/reference-data/background-message-key.enum";
-import { setMk } from "@popup/actions/mk.actions"
 import { BackgroundMessage } from "src/background/background-message.interface";
+import { BackgroundCommand } from "src/reference-data/background-message-key.enum";
+import { Screen } from "src/reference-data/screen.enum";
+import "./App.css";
+import { AddAccountRouterComponent } from "./pages/add-account/add-account-router/add-account-router.component";
 import { AppContainerComponent } from "./pages/app-container/app-container.component";
 import { ErrorMessageContainerComponent } from "./pages/error-message-container/error-message-container.component";
-import { AddAccountRouterComponent } from "./pages/add-account/add-account-router/add-account-router.component";
-import { Screen } from "src/reference-data/screen.enum";
+import { SignInComponent } from "./pages/sign-in/sign-in.component";
+import { SignUpComponent } from "./pages/sign-up/sign-up.component";
 
-const App = ({ setMk, mk, getAccounts, accounts, navigateTo }: PropsFromRedux) => {
+const App = ({
+  setMk,
+  mk,
+  getAccounts,
+  accounts,
+  navigateTo,
+}: PropsFromRedux) => {
   useEffect(() => {
     getAccounts();
   }, [getAccounts]);
 
   useEffect(() => {
-    chrome.runtime.sendMessage({command: BackgroundCommand.GET_MK});
+    chrome.runtime.sendMessage({ command: BackgroundCommand.GET_MK });
     chrome.runtime.onMessage.addListener(onSendBackListener);
-  }, [setMk])
-
-  const onSendBackListener = (message: BackgroundMessage) => {
-    if(message.command === BackgroundCommand.SEND_BACK_MK) {
-      setMk(message.value)
+  }, [setMk]);
+  useEffect(() => {
+    if (
+      // From testing the following conditions seem to indicate that the popup was opened on a secondary monitor
+      window.screenLeft < 0 ||
+      window.screenTop < 0 ||
+      window.screenLeft > window.screen.width ||
+      window.screenTop > window.screen.height
+    ) {
+      chrome.runtime.getPlatformInfo(function (info) {
+        if (info.os === "mac") {
+          const fontFaceSheet = new CSSStyleSheet();
+          fontFaceSheet.insertRule(`
+            @keyframes redraw {
+              0% {
+                opacity: 1;
+              }
+              100% {
+                opacity: .99;
+              }
+            }
+          `);
+          fontFaceSheet.insertRule(`
+            html {
+              animation: redraw 1s linear infinite;
+            }
+          `);
+          //@ts-ignore
+          document.adoptedStyleSheets = [
+            //@ts-ignore
+            ...document.adoptedStyleSheets,
+            fontFaceSheet,
+          ];
+        }
+      });
     }
-    chrome.runtime.onMessage.removeListener(onSendBackListener)
-  }
+  }, []);
+  const onSendBackListener = (message: BackgroundMessage) => {
+    if (message.command === BackgroundCommand.SEND_BACK_MK) {
+      setMk(message.value);
+    }
+    chrome.runtime.onMessage.removeListener(onSendBackListener);
+  };
 
   const renderMainLayoutNav = () => {
-    
-    if(!mk) {
-      if(accounts.length === 0) {
-        return <SignUpComponent />
+    if (!mk) {
+      if (accounts.length === 0) {
+        return <SignUpComponent />;
+      } else {
+        return <SignInComponent />;
       }
-      else {
-        return <SignInComponent />
-      }
-    }
-    else {
-      if(accounts.length === 0) {
+    } else {
+      if (accounts.length === 0) {
         navigateTo(Screen.ACCOUNT_PAGE_INIT_ACCOUNT);
-        return <AddAccountRouterComponent />
-      }
-      else {
-        return <AppContainerComponent />
+        return <AddAccountRouterComponent />;
+      } else {
+        return <AppContainerComponent />;
       }
     }
-  }
+  };
 
   return (
     <div className="App">
@@ -63,7 +100,7 @@ const App = ({ setMk, mk, getAccounts, accounts, navigateTo }: PropsFromRedux) =
 const mapStateToProps = (state: RootState) => {
   return {
     mk: state.mk,
-    accounts: state.accounts
+    accounts: state.accounts,
   };
 };
 
