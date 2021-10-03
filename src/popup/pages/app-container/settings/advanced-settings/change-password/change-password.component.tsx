26f30e40fd3450e1a402f064d979b0ad885895e6
Cedric Guillas <cedric.guillas78@gmail.com>
2021-10-03 12:54:55 +0800

    feat(change password): Implemented change password page

--- src/popup/pages/app-container/settings/advanced-settings/change-password/change-password.component.tsx
+++ src/popup/pages/app-container/settings/advanced-settings/change-password/change-password.component.tsx
@@ -1,25 +1,101 @@
+import { setErrorMessage } from '@popup/actions/message.actions';
+import { setMk } from '@popup/actions/mk.actions';
+import { navigateTo } from '@popup/actions/navigation.actions';
 import { RootState } from '@popup/store';
-import React from 'react';
+import React, { useState } from 'react';
 import { connect, ConnectedProps } from 'react-redux';
+import ButtonComponent from 'src/common-ui/button/button.component';
+import { InputType } from 'src/common-ui/input/input-type.enum';
+import InputComponent from 'src/common-ui/input/input.component';
 import { PageTitleComponent } from 'src/common-ui/page-title/page-title.component';
+import { Screen } from 'src/reference-data/screen.enum';
+import AccountUtils from 'src/utils/account.utils';
+import MkUtils from 'src/utils/mk.utils';
 import './change-password.component.scss';
 
-const ChangePassword = ({}: PropsFromRedux) => {
+const ChangePassword = ({
+  setErrorMessage,
+  setMk,
+  navigateTo,
+  accounts,
+  mk,
+}: PropsFromRedux) => {
+  const [oldPassword, setOldPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [newPasswordConfirm, setNewPasswordConfirm] = useState('');
+
+  const submitMk = (): any => {
+    if (mk !== oldPassword) {
+      setErrorMessage('wrong_password');
+      return;
+    }
+    if (newPassword === newPasswordConfirm) {
+      if (MkUtils.isPasswordValid(newPassword)) {
+        AccountUtils.saveAccounts(accounts, newPassword);
+        setMk(newPassword);
+        navigateTo(Screen.HOME_PAGE, true);
+      } else {
+        setErrorMessage('popup_password_regex');
+      }
+    } else {
+      setErrorMessage('popup_password_mismatch');
+    }
+  };
+
   return (
     <div className="change-password-page">
       <PageTitleComponent
         title="popup_html_change_password"
         isBackButtonEnabled={true}
       />
+
+      <div className="caption">
+        {chrome.i18n.getMessage('popup_html_change_password_text')}
+      </div>
+
+      <InputComponent
+        value={oldPassword}
+        onChange={setOldPassword}
+        logo="lock"
+        placeholder="popup_html_old_password"
+        type={InputType.PASSWORD}
+        onEnterPress={submitMk}
+      />
+      <InputComponent
+        value={newPassword}
+        onChange={setNewPassword}
+        logo="lock"
+        placeholder="popup_html_new_password"
+        type={InputType.PASSWORD}
+        onEnterPress={submitMk}
+      />
+      <InputComponent
+        value={newPasswordConfirm}
+        onChange={setNewPasswordConfirm}
+        logo="lock"
+        placeholder="popup_html_confirm"
+        type={InputType.PASSWORD}
+        onEnterPress={submitMk}
+      />
+
+      <ButtonComponent
+        label={'popup_html_submit'}
+        logo="submit"
+        onClick={submitMk}
+      />
     </div>
   );
 };
 
 const mapStateToProps = (state: RootState) => {
-  return {};
+  return { mk: state.mk, accounts: state.accounts };
 };
 
-const connector = connect(mapStateToProps, {});
+const connector = connect(mapStateToProps, {
+  setErrorMessage,
+  setMk,
+  navigateTo,
+});
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
 export const ChangePasswordComponent = connector(ChangePassword);
