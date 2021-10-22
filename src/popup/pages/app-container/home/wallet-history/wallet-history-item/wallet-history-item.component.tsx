aa49d63b913fb4c4ead338e30c1507d9e869f505
Cedric Guillas <cedric.guillas78@gmail.com>
2021-10-22 17:31:16 +0800

    feat(wallet history): Work in progress

---
+++ src/popup/pages/app-container/home/wallet-history/wallet-history-item/wallet-history-item.component.tsx
@@ -0,0 +1,65 @@
+import { Transaction } from '@interfaces/transaction.interface';
+import { RootState } from '@popup/store';
+import moment from 'moment';
+import React, { useState } from 'react';
+import { connect, ConnectedProps } from 'react-redux';
+import './wallet-history-item.component.scss';
+
+interface WalletHistoryItemProps {
+  transaction: Transaction;
+}
+
+const WalletHistoryItem = ({
+  transaction,
+  activeAccountName,
+}: PropsFromRedux) => {
+  const [isMemoOpened, setIsMemoOpened] = useState(false);
+
+  return (
+    <div
+      className="wallet-history-item"
+      onClick={() => setIsMemoOpened(!isMemoOpened)}>
+      <div className="transaction" key={transaction.key}>
+        <div className="information-panel">
+          <div className="top-row">
+            <div className="date">
+              {moment(transaction.timestamp).format('L')}
+            </div>
+            <div className="amount">
+              {activeAccountName === transaction.from ? '-' : '+'}{' '}
+              {transaction.amount.split(' ')[0]}
+            </div>
+          </div>
+          <div className="bottom-row">
+            <div className="from-to">
+              {chrome.i18n.getMessage(
+                activeAccountName === transaction.from
+                  ? 'popup_html_transfer_to'
+                  : 'popup_html_transfer_from',
+              )}
+              : @
+              {activeAccountName === transaction.from
+                ? transaction.to
+                : transaction.from}
+            </div>
+            <div className="currency">{transaction.amount.split(' ')[1]}</div>
+          </div>
+        </div>
+        <div className={'memo-panel ' + isMemoOpened ? 'opened' : 'closed'}>
+          {transaction.memo}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state: RootState) => {
+  return {
+    activeAccountName: state.activeAccount.name,
+  };
+};
+
+const connector = connect(mapStateToProps, {});
+type PropsFromRedux = ConnectedProps<typeof connector> & WalletHistoryItemProps;
+
+export const WalletHistoryItemComponent = connector(WalletHistoryItem);
--- src/popup/pages/app-container/home/wallet-history/wallet-history.component.tsx
+++ src/popup/pages/app-container/home/wallet-history/wallet-history.component.tsx
@@ -1,6 +1,6 @@
 import { Transaction, Transactions } from '@interfaces/transaction.interface';
+import { WalletHistoryItemComponent } from '@popup/pages/app-container/home/wallet-history/wallet-history-item/wallet-history-item.component';
 import { RootState } from '@popup/store';
-import moment from 'moment';
 import React from 'react';
 import { connect, ConnectedProps } from 'react-redux';
 import { PageTitleComponent } from 'src/common-ui/page-title/page-title.component';
@@ -13,38 +13,12 @@ const WalletHistory = ({ transactions, activeAccountName }: PropsFromRedux) => {
         title="popup_html_wallet_history"
         isBackButtonEnabled={true}
       />
+
       <div className="page-content">
         {transactions.list.map((transaction: Transaction) => (
-          <div className="transaction" key={transaction.key}>
-            <div className="information-panel">
-              <div className="top-row">
-                <div className="date">
-                  {moment(transaction.timestamp).format('L')}
-                </div>
-                <div className="amount">
-                  {activeAccountName === transaction.from ? '-' : '+'}{' '}
-                  {transaction.amount.split(' ')[0]}
-                </div>
-              </div>
-              <div className="bottom-row">
-                <div className="from-to">
-                  {chrome.i18n.getMessage(
-                    activeAccountName === transaction.from
-                      ? 'popup_html_transfer_to'
-                      : 'popup_html_transfer_from',
-                  )}
-                  : @
-                  {activeAccountName === transaction.from
-                    ? transaction.to
-                    : transaction.from}
-                </div>
-                <div className="currency">
-                  {transaction.amount.split(' ')[1]}
-                </div>
-              </div>
-            </div>
-            <div className="memo-panel">{transaction.memo}</div>
-          </div>
+          <WalletHistoryItemComponent
+            key={transaction.key}
+            transaction={transaction}></WalletHistoryItemComponent>
         ))}
       </div>
     </div>
