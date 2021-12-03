6f077cfb0eb8cbacef60d8394c3916bcfa0bed50
Cedric Guillas <cedric.guillas78@gmail.com>
2021-12-03 17:29:15 +0800

    fix import settings

--- src/background/settings.module.ts
+++ src/background/settings.module.ts
@@ -1,10 +1,134 @@
-import { BackgroundCommand } from '@reference-data/background-message-key.enum';
+import { LocalStorageClaimItem } from '@interfaces/local-storage-claim-item.interface';
+import { NoConfirm } from '@interfaces/no-confirm.interface';
+import { Rpc } from '@interfaces/rpc.interface';
+import { Settings } from '@interfaces/settings.interface';
+import { TransferToItems } from '@interfaces/transfer-to-username.interface';
+import { LocalStorageKeyEnum } from '@reference-data/local-storage-key.enum';
+import LocalStorageUtils from 'src/utils/localStorage.utils';
 
-const sendBackImportedFileContent = (fileContent: any) => {
-  chrome.runtime.sendMessage({
-    command: BackgroundCommand.SEND_BACK_IMPORTED_ACCOUNTS,
-    value: fileContent,
-  });
+const sendBackImportedFileContent = async (fileContent: any) => {
+  const settings: Settings = fileContent;
+
+  if (
+    settings &&
+    !Object.values(LocalStorageKeyEnum).includes(
+      Object.keys(settings)[0] as LocalStorageKeyEnum,
+    )
+  ) {
+    const importedNoConfirm: any = settings;
+    let existingNoConfirm: NoConfirm =
+      await LocalStorageUtils.getValueFromLocalStorage(
+        LocalStorageKeyEnum.NO_CONFIRM,
+      );
+
+    if (!existingNoConfirm) existingNoConfirm = {};
+    for (const username of Object.keys(importedNoConfirm)) {
+      if (!existingNoConfirm[username]) existingNoConfirm[username] = {};
+      for (const website of Object.keys(importedNoConfirm[username])) {
+        existingNoConfirm[username][website] =
+          importedNoConfirm[username][website];
+      }
+    }
+    await LocalStorageUtils.saveValueInLocalStorage(
+      LocalStorageKeyEnum.NO_CONFIRM,
+      existingNoConfirm,
+    );
+  } else {
+    if (settings.autolock) {
+      await LocalStorageUtils.saveValueInLocalStorage(
+        LocalStorageKeyEnum.AUTOLOCK,
+        settings.autolock,
+      );
+    }
+    if (settings.claimAccounts) {
+      let existingClaimsAccounts: LocalStorageClaimItem =
+        await LocalStorageUtils.getValueFromLocalStorage(
+          LocalStorageKeyEnum.CLAIM_ACCOUNTS,
+        );
+      if (!existingClaimsAccounts) existingClaimsAccounts = {};
+      for (const username of Object.keys(settings.claimAccounts)) {
+        existingClaimsAccounts[username] = settings.claimAccounts[username];
+      }
+      await LocalStorageUtils.saveValueInLocalStorage(
+        LocalStorageKeyEnum.CLAIM_ACCOUNTS,
+        existingClaimsAccounts,
+      );
+    }
+
+    if (settings.claimRewards) {
+      let existingClaimsRewards: LocalStorageClaimItem =
+        await LocalStorageUtils.getValueFromLocalStorage(
+          LocalStorageKeyEnum.CLAIM_REWARDS,
+        );
+      if (!existingClaimsRewards) existingClaimsRewards = {};
+      for (const username of Object.keys(settings.claimRewards)) {
+        existingClaimsRewards[username] = settings.claimRewards[username];
+      }
+      await LocalStorageUtils.saveValueInLocalStorage(
+        LocalStorageKeyEnum.CLAIM_REWARDS,
+        existingClaimsRewards,
+      );
+    }
+
+    if (settings.keychainify_enabled) {
+      await LocalStorageUtils.saveValueInLocalStorage(
+        LocalStorageKeyEnum.KEYCHAINIFY_ENABLED,
+        settings.keychainify_enabled,
+      );
+    }
+
+    if (settings.no_confirm) {
+      let existingNoConfirm: NoConfirm =
+        await LocalStorageUtils.getValueFromLocalStorage(
+          LocalStorageKeyEnum.NO_CONFIRM,
+        );
+
+      if (!existingNoConfirm) existingNoConfirm = {};
+      for (const username of Object.keys(settings.no_confirm)) {
+        if (!existingNoConfirm[username]) existingNoConfirm[username] = {};
+        for (const website of Object.keys(settings.no_confirm[username])) {
+          existingNoConfirm[username][website] =
+            settings.no_confirm[username][website];
+        }
+      }
+      await LocalStorageUtils.saveValueInLocalStorage(
+        LocalStorageKeyEnum.NO_CONFIRM,
+        existingNoConfirm,
+      );
+    }
+
+    if (settings.rpc) {
+      let existingRpc: Rpc[] = await LocalStorageUtils.getValueFromLocalStorage(
+        LocalStorageKeyEnum.RPC_LIST,
+      );
+      if (!existingRpc) existingRpc = [];
+      await LocalStorageUtils.saveValueInLocalStorage(
+        LocalStorageKeyEnum.RPC_LIST,
+        [...existingRpc, settings.rpc],
+      );
+    }
+
+    if (settings.transfer_to) {
+      let existingTransferTo: TransferToItems =
+        await LocalStorageUtils.getValueFromLocalStorage(
+          LocalStorageKeyEnum.TRANSFER_TO_USERNAMES,
+        );
+
+      if (!existingTransferTo) existingTransferTo = {};
+      for (const username of Object.keys(settings.transfer_to)) {
+        existingTransferTo[username] = settings.transfer_to[username];
+      }
+      await LocalStorageUtils.saveValueInLocalStorage(
+        LocalStorageKeyEnum.TRANSFER_TO_USERNAMES,
+        existingTransferTo,
+      );
+    }
+  }
+
+  // chrome.runtime.sendMessage({
+  //   command: BackgroundCommand.SEND_BACK_IMPORTED_ACCOUNTS,
+  //   value: fileContent,
+  // });
 };
 
 const SettingsModule = {
