82e6b92e5e76ef1ca5b21f06ef762108a59e874b
stoodkev <quentin.correa@gmail.com>
2021-12-03 14:22:16 +0800

    add custom json operation

---
+++ src/background/requests/operations/ops/custom-json.ts
@@ -0,0 +1,51 @@
+import { getRequestHandler } from '@background/requests';
+import {
+  beautifyErrorMessage,
+  createMessage,
+} from '@background/requests/operations/operations.utils';
+import { PrivateKey } from '@hiveio/dhive';
+import {
+  KeychainKeyTypesLC,
+  RequestCustomJSON,
+  RequestId,
+} from '@interfaces/keychain.interface';
+
+export const broadcastCustomJson = async (
+  data: RequestCustomJSON & RequestId,
+) => {
+  const client = getRequestHandler().getHiveClient();
+  const key = getRequestHandler().key;
+  let result, err;
+
+  try {
+    result = await client.broadcast.json(
+      {
+        required_auths:
+          data.method.toLowerCase() === KeychainKeyTypesLC.active
+            ? [data.username!]
+            : [],
+        required_posting_auths:
+          data.method.toLowerCase() === KeychainKeyTypesLC.posting
+            ? [data.username!]
+            : [],
+        id: data.id,
+        json: data.json,
+      },
+      PrivateKey.from(key!),
+    );
+  } catch (e) {
+    err = e;
+  }
+
+  const err_message = beautifyErrorMessage(err);
+
+  const message = createMessage(
+    err,
+    result,
+    data,
+    chrome.i18n.getMessage('bgd_ops_broadcast'),
+    err_message,
+  );
+  console.log('m', message);
+  return message;
+};
