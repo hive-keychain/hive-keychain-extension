ecd99def6d9474a3b3ad5181105a185e016eeae8
Cedric Gui <cedric.guillas78@gmail.com>
2022-01-20 08:17:26 +0800

    feat(checkbox): Added alignment option to checkbox

--- src/common-ui/checkbox/checkbox.component.tsx
+++ src/common-ui/checkbox/checkbox.component.tsx
@@ -2,6 +2,11 @@ import { Checkbox } from 'pretty-checkbox-react';
 import React from 'react';
 import './checkbox.component.scss';
 
+enum LabelAlignment {
+  CENTER = 'align-center',
+  TOP = 'align-top',
+}
+
 interface CheckboxProps {
   onChange: (value: boolean) => void;
   title?: string;
@@ -9,11 +14,15 @@ interface CheckboxProps {
   skipTranslation?: boolean;
   hint?: string;
   skipHintTranslation?: boolean;
+  alignment?: boolean;
 }
 
 const CheckboxComponent = (props: CheckboxProps) => {
   return (
-    <div className="checkbox-container">
+    <div
+      className={`checkbox-container ${
+        props.alignment ? props.alignment : LabelAlignment.TOP
+      }`}>
       <div className="checkbox-and-label">
         <Checkbox
           checked={props.checked}
