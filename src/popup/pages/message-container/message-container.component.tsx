9d21fbeb3a0a35ee1d29120bba5b7232f05c5fa9
Cedric Gui <cedric.guillas78@gmail.com>
2022-01-27 16:54:03 +0800

    fix(progress bar): work in progress

--- src/popup/pages/message-container/message-container.component.tsx
+++ src/popup/pages/message-container/message-container.component.tsx
@@ -4,35 +4,49 @@ import React, { useEffect, useState } from 'react';
 import { connect, ConnectedProps } from 'react-redux';
 import './message-container.component.scss';
 
+const DURATION = 20;
+
 const MessageContainer = ({ errorMessage, resetMessage }: PropsFromRedux) => {
-  const [progressBarWidth, setProgressBarWidth] = useState(0);
   const [timeoutId, setTimeoutId] = useState<any>();
+  const [isFull, setIsFull] = useState(false);
+  const [progressVal, setProgressVal] = useState(0);
+  const [intervalId, setIntervalId] = useState<any>();
   useEffect(() => {
     if (errorMessage.key) {
-      setProgressBarWidth(100);
-      const t = setTimeout(() => {
+      const step = 100 / DURATION;
+      const int = setInterval(() => {
+        console.log('progress val', progressVal);
+        console.log(progressVal + step);
+        setProgressVal(progressVal + step);
+      }, 1000);
+      setIntervalId(int);
+
+      const to = setTimeout(() => {
+        console.log('ici');
+        clearInterval(intervalId);
         resetMessage();
-        setProgressBarWidth(0);
-      }, 5000);
-      setTimeoutId(t);
+        setProgressVal(0);
+      }, DURATION * 1000);
+      setTimeoutId(to);
     }
   }, [errorMessage]);
 
   const close = () => {
+    clearInterval(intervalId);
     clearTimeout(timeoutId);
     resetMessage();
-    setProgressBarWidth(0);
+    setProgressVal(0);
   };
 
   return (
     <div className="message-container" onClick={() => close()}>
       {errorMessage.key.length > 0 && (
         <div className={`container ${errorMessage.type}`}>
-          <div className="barHolder">
-            <div
-              className="bar"
-              style={{ width: progressBarWidth + '%' }}></div>
-          </div>
+          {/* <div className="barHolder">
+            <div className={`bar ${isFull ? 'full' : ''}`}></div>
+          </div> */}
+          <progress value={progressVal} max="100"></progress>
+
           <div className="message">
             {chrome.i18n.getMessage(errorMessage.key, errorMessage.params)}
           </div>
