51d559fa0df1d8164a603792e399e82d88de2da6
stoodkev <quentin.correa@gmail.com>
2022-01-28 18:41:06 +0800

    fix autolock

--- src/background/autolock.module.ts
+++ src/background/autolock.module.ts
@@ -3,32 +3,47 @@ import { BackgroundCommand } from '@reference-data/background-message-key.enum';
 import Logger from 'src/utils/logger.utils';
 import { Autolock, AutoLockType } from '../interfaces/autolock.interface';
 
-let idleListenerReady = false;
+let listener: any;
 
-const startAutolock = (autoLock: Autolock) => {
+const startAutolock = async (autoLock: Autolock) => {
   //Receive autoLock from the popup (upon registration or unlocking)
+  Logger.info('Starting autolock');
+  if (listener) {
+    chrome.idle.onStateChanged.removeListener(listener);
+  }
+  listener = (state: any) => {
+    if (
+      (autoLock.type === AutoLockType.DEVICE_LOCK &&
+        state === AutoLockType.DEVICE_LOCK) ||
+      (autoLock.type === AutoLockType.IDLE_LOCK && state !== 'active')
+    ) {
+      Logger.info(
+        `hive-keychain: locking because ${
+          autoLock.type === AutoLockType.DEVICE_LOCK
+            ? 'computer locked'
+            : 'computer is idle'
+        }`,
+      );
+      MkModule.resetMk();
+      chrome.runtime.sendMessage({
+        command: BackgroundCommand.LOCK_APP,
+      });
+    }
+  };
+
   if (
-    idleListenerReady === false &&
     autoLock &&
     autoLock.type !== AutoLockType.DEFAULT &&
     (autoLock.type === AutoLockType.DEVICE_LOCK ||
       autoLock.type === AutoLockType.IDLE_LOCK)
   ) {
-    Logger.info('hive-keychain: setting up idle listener');
+    Logger.info(
+      `hive-keychain: setting up ${autoLock.type} listener ${
+        autoLock.type === AutoLockType.IDLE_LOCK ? `(${autoLock.mn}mn)` : ''
+      }`,
+    );
     chrome.idle.setDetectionInterval(autoLock.mn * 60);
-    chrome.idle.onStateChanged.addListener((state) => {
-      if (
-        (autoLock.type === 'locked' && state === 'locked') ||
-        (autoLock.type === 'idle' && state !== 'active')
-      ) {
-        Logger.info('hive-keychain: locking because computer locked');
-        MkModule.resetMk();
-        chrome.runtime.sendMessage({
-          command: BackgroundCommand.LOCK_APP,
-        });
-      }
-    });
-    idleListenerReady = true;
+    chrome.idle.onStateChanged.addListener(listener);
   }
 };
 
