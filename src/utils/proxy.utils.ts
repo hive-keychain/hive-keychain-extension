5deae36e2a25d769d2d3938af1d689d86fd09b42
cedricguillas <cedric.guillas78@gmail.com>
2022-04-15 14:19:42 +0800

    feat(proposal tab): Added proxy votes (#128)

---
+++ src/utils/proxy.utils.ts
@@ -0,0 +1,20 @@
+import { ExtendedAccount } from '@hiveio/dhive';
+import AccountUtils from 'src/utils/account.utils';
+
+const findUserProxy = async (user: ExtendedAccount): Promise<string | null> => {
+  const previousChecked: string[] = [user.name!];
+  if (user.proxy.length === 0) return null;
+  else {
+    let proxy = user.proxy;
+    do {
+      if (previousChecked.includes(proxy)) return null;
+      previousChecked.push(proxy);
+      proxy = (await AccountUtils.getExtendedAccount(proxy)).proxy;
+    } while (proxy.length === 0);
+    return proxy;
+  }
+};
+
+const ProxyUtils = { findUserProxy };
+
+export default ProxyUtils;
