64b96fa4458a23542c55b2386f0e88ba1da2abdb
Saturno Mangieri <saturnob612@gmail.com>
2022-05-06 13:01:15 -0400

    Uncommenting lines of code that works

--- src/__tests__/utils/active-accounts.utils.test.ts
+++ src/__tests__/utils/active-accounts.utils.test.ts
@@ -1,5 +1,6 @@
 import { ActiveAccount } from '@interfaces/active-account.interface';
 import ActiveAccountUtils from 'src/utils/active-account.utils';
+import FormatUtils from 'src/utils/format.utils';
 //chrome
 const chrome = require('chrome-mock');
 global.chrome = chrome;
@@ -24,25 +25,25 @@ describe(' active-account.utils tests', () => {
     });
   });
 
-  // describe.skip('getValFromString tests', () => {
-  //   const stringCase1 = '12.000 HIVE';
-  //   const stringCase2 = '10.000 JustAnother.string';
-  //   const stringToFail = 'Hello HBD';
-  //   test(`passing an expected string formatted as ${stringCase1} must return a number = 12`, () => {
-  //     const numbeResult = ActiveAccountUtils.getValFromString(stringCase1);
-  //     expect(numbeResult).toBe(12);
-  //   });
-  //   test(`passing an expected string formatted as ${stringCase2} must return a number = 12`, () => {
-  //     const numbeResult = ActiveAccountUtils.getValFromString(stringCase2);
-  //     expect(numbeResult).toBe(10);
-  //   });
-  //   test(`passing an non expected string formatted as ${stringToFail} must return NaN`, () => {
-  //     //Note: if the expected input will always be formatted as required this is an extra case
-  //     //otherwise a validation may be required.
-  //     const numbeResult = ActiveAccountUtils.getValFromString(stringToFail);
-  //     expect(numbeResult).toBe(NaN);
-  //   });
-  // });
+  describe.skip('getValFromString tests', () => {
+    const stringCase1 = '12.000 HIVE';
+    const stringCase2 = '10.000 JustAnother.string';
+    const stringToFail = 'Hello HBD';
+    test(`passing an expected string formatted as ${stringCase1} must return a number = 12`, () => {
+      const numbeResult = ActiveAccountUtils.getValFromString(stringCase1);
+      expect(numbeResult).toBe(12);
+    });
+    test(`passing an expected string formatted as ${stringCase2} must return a number = 12`, () => {
+      const numbeResult = ActiveAccountUtils.getValFromString(stringCase2);
+      expect(numbeResult).toBe(10);
+    });
+    test(`passing an non expected string formatted as ${stringToFail} must return NaN`, () => {
+      //Note: if the expected input will always be formatted as required this is an extra case
+      //otherwise a validation may be required.
+      const numbeResult = ActiveAccountUtils.getValFromString(stringToFail);
+      expect(numbeResult).toBe(NaN);
+    });
+  });
 
   describe('hasRewards tests without mocking functions', () => {
     //Note as mocking the functions is required
@@ -190,7 +191,7 @@ describe(' active-account.utils tests', () => {
     });
   });
 
-  describe.skip('hasRewards tests with mocking functions', () => {
+  describe.only('hasRewards tests with mocking functions', () => {
     //NOTE it is been skipped and need research to find a way to test
     // the return mockec value within another function call.
 
@@ -199,16 +200,15 @@ describe(' active-account.utils tests', () => {
     test('passing Not rewards on rewardsObj, must return false', () => {
       const rewardsObj = {
         reward_hbd: '1.00 HDB',
-        reward_hp: '1.00 HP',
-        reward_hive: '1.00 HIVE',
+        reward_hp: '0.00 HP',
+        reward_hive: '0.00 HIVE',
       };
 
-      ActiveAccountUtils.getValFromString = jest
-        .fn()
-        .mockReturnValue(console.log('hi from getValue'));
-      // ActiveAccountUtils.getValFromString = jest
-      //   .fn()
-      //   .mockImplementation(() => console.log('HI THERE!'));
+      jest
+        .spyOn(ActiveAccountUtils, 'getValFromString')
+        .mockReturnValueOnce(0)
+        .mockReturnValueOnce(0)
+        .mockReturnValueOnce(0);
 
       expect(
         ActiveAccountUtils.hasReward(
@@ -268,106 +268,103 @@ describe(' active-account.utils tests', () => {
     });
   });
 
-  // describe('getAvailableRewards tests', () => {
-  //   //NOTE: So far i cannot mock chrome.i18n.getMessage and I get an error:
-  //   // -> TypeError: Cannot set property getMessage of #<Object> which has only a getter
-  //   //
-  //   //mock getValFromString, chrome.getMessage, FormatUtils.toHP, activeAccount to make sure test is stable
-  //   test('passing an active account object with all rewards must return especific format as seen on expectedRewardText bellow', () => {
-  //     const justRewardsObj = {
-  //       account: {
-  //         reward_hbd_balance: '1.00 HBD',
-  //         reward_vesting_balance: '1.000000 VESTS',
-  //         reward_hive_balance: '1.00 HIVE',
-  //       },
-  //     } as ActiveAccount;
-  //     const messageI18n = 'Message';
-  //     const expectedRewardText = `["1.00 HBD","1 HP","1.00 HIVE","${messageI18n}:<br>1 HP / 1.00 HBD / 1.00 HIVE"]`;
-  //     Formatutils.toHP = jest.fn().mockReturnValue(1); // so it will print 1 HP.
-  //     ActiveAccountUtils.getValFromString = jest.fn().mockReturnValue(1);
-  //     chrome.i18n.getMessage = jest
-  //       .fn()
-  //       .mockImplementation(() => `${messageI18n}`);
-  //     const resultBalances =
-  //       ActiveAccountUtils.getAvailableRewards(justRewardsObj);
-  //     expect(JSON.stringify(resultBalances)).toBe(expectedRewardText);
-  //   });
-  //   test('passing an active account object with 1 reward(reward_hbd_balance) must return especific format as seen on expectedRewardText bellow', () => {
-  //     const justRewardsObj = {
-  //       account: {
-  //         reward_hbd_balance: '1.00 HBD',
-  //         reward_vesting_balance: '0.000000 VESTS',
-  //         reward_hive_balance: '0.00 HIVE',
-  //       },
-  //     } as ActiveAccount;
-  //     const messageI18n = 'Message 2';
-  //     const expectedRewardText = `["1.00 HBD","0 HP","0.00 HIVE","${messageI18n}:<br>1.00 HBD"]`;
-  //     Formatutils.toHP = jest.fn().mockReturnValue(0); // so it won't print any HP.
-  //     ActiveAccountUtils.getValFromString = jest.fn().mockReturnValue(1);
-  //     chrome.i18n.getMessage = jest
-  //       .fn()
-  //       .mockImplementation(() => `${messageI18n}`);
-  //     const resultBalances =
-  //       ActiveAccountUtils.getAvailableRewards(justRewardsObj);
-  //     expect(JSON.stringify(resultBalances)).toBe(expectedRewardText);
-  //   });
-  //   test('passing an active account object with 1 reward(reward_vesting_balance) must return especific format as seen on expectedRewardText bellow', () => {
-  //     const justRewardsObj = {
-  //       account: {
-  //         reward_hbd_balance: '0.00 HBD',
-  //         reward_vesting_balance: '10.000000 VESTS',
-  //         reward_hive_balance: '0.00 HIVE',
-  //       },
-  //     } as ActiveAccount;
-  //     const messageI18n = 'Message 3';
-  //     const expectedRewardText = `["0.00 HBD","10 HP","0.00 HIVE","${messageI18n}:<br>10 HP"]`;
-  //     Formatutils.toHP = jest.fn().mockReturnValue(10); // so it will print 10 HP.
-  //     ActiveAccountUtils.getValFromString = jest.fn().mockReturnValue(1);
-  //     chrome.i18n.getMessage = jest
-  //       .fn()
-  //       .mockImplementation(() => `${messageI18n}`);
-  //     const resultBalances =
-  //       ActiveAccountUtils.getAvailableRewards(justRewardsObj);
-  //     expect(JSON.stringify(resultBalances)).toBe(expectedRewardText);
-  //   });
-  //   test('passing an active account object with 1 reward(reward_hive_balance) must return especific format as seen on expectedRewardText bellow', () => {
-  //     const justRewardsObj = {
-  //       account: {
-  //         reward_hbd_balance: '0.00 HBD',
-  //         reward_vesting_balance: '0.000000 VESTS',
-  //         reward_hive_balance: '100.00 HIVE',
-  //       },
-  //     } as ActiveAccount;
-  //     const messageI18n = 'Message 4';
-  //     const expectedRewardText = `["0.00 HBD","0 HP","100.00 HIVE","${messageI18n}:<br>100.00 HIVE"]`;
-  //     Formatutils.toHP = jest.fn().mockReturnValue(0); // so it will print 0 HP.
-  //     ActiveAccountUtils.getValFromString = jest.fn().mockReturnValue(1);
-  //     chrome.i18n.getMessage = jest
-  //       .fn()
-  //       .mockImplementation(() => `${messageI18n}`);
-  //     const resultBalances =
-  //       ActiveAccountUtils.getAvailableRewards(justRewardsObj);
-  //     expect(JSON.stringify(resultBalances)).toBe(expectedRewardText);
-  //   });
-  //   //optional test
-  //   test('passing an active account object with NaN reward values will return those values without validation in the same format as above', () => {
-  //     const justRewardsObj = {
-  //       account: {
-  //         reward_hbd_balance: 'A HBD',
-  //         reward_vesting_balance: 'B VESTS',
-  //         reward_hive_balance: 'WT HIVE',
-  //       },
-  //     } as ActiveAccount;
-  //     const messageI18n = 'Message 5';
-  //     const expectedRewardText = `["A HBD","0 HP","WT HIVE","${messageI18n}:<br>A HBD / WT HIVE"]`;
-  //     Formatutils.toHP = jest.fn().mockReturnValue(0); // so it will print 0 HP.
-  //     ActiveAccountUtils.getValFromString = jest.fn().mockReturnValue(1);
-  //     chrome.i18n.getMessage = jest
-  //       .fn()
-  //       .mockImplementation(() => `${messageI18n}`);
-  //     const resultBalances =
-  //       ActiveAccountUtils.getAvailableRewards(justRewardsObj);
-  //     expect(JSON.stringify(resultBalances)).toBe(expectedRewardText);
-  //   });
-  // });
+  describe('getAvailableRewards tests', () => {
+    //mock getValFromString, chrome.getMessage, FormatUtils.toHP, activeAccount to make sure test is stable
+    test('passing an active account object with all rewards must return especific format as seen on expectedRewardText bellow', () => {
+      const justRewardsObj = {
+        account: {
+          reward_hbd_balance: '1.00 HBD',
+          reward_vesting_balance: '1.000000 VESTS',
+          reward_hive_balance: '1.00 HIVE',
+        },
+      } as ActiveAccount;
+      const messageI18n = 'Message';
+      const expectedRewardText = `["1.00 HBD","1 HP","1.00 HIVE","${messageI18n}:<br>1 HP / 1.00 HBD / 1.00 HIVE"]`;
+      FormatUtils.toHP = jest.fn().mockReturnValue(1); // so it will print 1 HP.
+      ActiveAccountUtils.getValFromString = jest.fn().mockReturnValue(1);
+      chrome.i18n.getMessage = jest
+        .fn()
+        .mockImplementation(() => `${messageI18n}`);
+      const resultBalances =
+        ActiveAccountUtils.getAvailableRewards(justRewardsObj);
+      expect(JSON.stringify(resultBalances)).toBe(expectedRewardText);
+    });
+    test('passing an active account object with 1 reward(reward_hbd_balance) must return especific format as seen on expectedRewardText bellow', () => {
+      const justRewardsObj = {
+        account: {
+          reward_hbd_balance: '1.00 HBD',
+          reward_vesting_balance: '0.000000 VESTS',
+          reward_hive_balance: '0.00 HIVE',
+        },
+      } as ActiveAccount;
+      const messageI18n = 'Message 2';
+      const expectedRewardText = `["1.00 HBD","0 HP","0.00 HIVE","${messageI18n}:<br>1.00 HBD"]`;
+      FormatUtils.toHP = jest.fn().mockReturnValue(0); // so it won't print any HP.
+      ActiveAccountUtils.getValFromString = jest.fn().mockReturnValue(1);
+      chrome.i18n.getMessage = jest
+        .fn()
+        .mockImplementation(() => `${messageI18n}`);
+      const resultBalances =
+        ActiveAccountUtils.getAvailableRewards(justRewardsObj);
+      expect(JSON.stringify(resultBalances)).toBe(expectedRewardText);
+    });
+    test('passing an active account object with 1 reward(reward_vesting_balance) must return especific format as seen on expectedRewardText bellow', () => {
+      const justRewardsObj = {
+        account: {
+          reward_hbd_balance: '0.00 HBD',
+          reward_vesting_balance: '10.000000 VESTS',
+          reward_hive_balance: '0.00 HIVE',
+        },
+      } as ActiveAccount;
+      const messageI18n = 'Message 3';
+      const expectedRewardText = `["0.00 HBD","10 HP","0.00 HIVE","${messageI18n}:<br>10 HP"]`;
+      FormatUtils.toHP = jest.fn().mockReturnValue(10); // so it will print 10 HP.
+      ActiveAccountUtils.getValFromString = jest.fn().mockReturnValue(1);
+      chrome.i18n.getMessage = jest
+        .fn()
+        .mockImplementation(() => `${messageI18n}`);
+      const resultBalances =
+        ActiveAccountUtils.getAvailableRewards(justRewardsObj);
+      expect(JSON.stringify(resultBalances)).toBe(expectedRewardText);
+    });
+    test('passing an active account object with 1 reward(reward_hive_balance) must return especific format as seen on expectedRewardText bellow', () => {
+      const justRewardsObj = {
+        account: {
+          reward_hbd_balance: '0.00 HBD',
+          reward_vesting_balance: '0.000000 VESTS',
+          reward_hive_balance: '100.00 HIVE',
+        },
+      } as ActiveAccount;
+      const messageI18n = 'Message 4';
+      const expectedRewardText = `["0.00 HBD","0 HP","100.00 HIVE","${messageI18n}:<br>100.00 HIVE"]`;
+      FormatUtils.toHP = jest.fn().mockReturnValue(0); // so it will print 0 HP.
+      ActiveAccountUtils.getValFromString = jest.fn().mockReturnValue(1);
+      chrome.i18n.getMessage = jest
+        .fn()
+        .mockImplementation(() => `${messageI18n}`);
+      const resultBalances =
+        ActiveAccountUtils.getAvailableRewards(justRewardsObj);
+      expect(JSON.stringify(resultBalances)).toBe(expectedRewardText);
+    });
+    //optional test
+    test('passing an active account object with NaN reward values will return those values without validation in the same format as above', () => {
+      const justRewardsObj = {
+        account: {
+          reward_hbd_balance: 'A HBD',
+          reward_vesting_balance: 'B VESTS',
+          reward_hive_balance: 'WT HIVE',
+        },
+      } as ActiveAccount;
+      const messageI18n = 'Message 5';
+      const expectedRewardText = `["A HBD","0 HP","WT HIVE","${messageI18n}:<br>A HBD / WT HIVE"]`;
+      FormatUtils.toHP = jest.fn().mockReturnValue(0); // so it will print 0 HP.
+      ActiveAccountUtils.getValFromString = jest.fn().mockReturnValue(1);
+      chrome.i18n.getMessage = jest
+        .fn()
+        .mockImplementation(() => `${messageI18n}`);
+      const resultBalances =
+        ActiveAccountUtils.getAvailableRewards(justRewardsObj);
+      expect(JSON.stringify(resultBalances)).toBe(expectedRewardText);
+    });
+  });
 });
