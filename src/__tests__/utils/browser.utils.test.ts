c5bcc58576658506bbe5e57c9a8bfcebca1053c3
Saturno Mangieri <saturnob612@gmail.com>
2022-05-09 15:23:18 -0400

    Tests browser.utils completed

---
+++ src/__tests__/utils/browser.utils.test.ts
@@ -0,0 +1,31 @@
+import BrowserUtils from 'src/utils/browser.utils';
+const env = process.env;
+
+describe('sendResponse tests', () => {
+  test('While mocking the env variable IS_FIREFOX it must return resolvedMessage', async () => {
+    //mocking env
+    process.env = {
+      ...env,
+      IS_FIREFOX: 'true',
+    };
+    const resolvedMessage = 'It is been returned';
+    const consoleLogA = (a?: any) => console.log(`Passed: ${a}`);
+    const result = await BrowserUtils.sendResponse(
+      resolvedMessage,
+      consoleLogA,
+    );
+    expect(result).not.toBeUndefined();
+    expect(result).toBe('It is been returned');
+    //returning env to its initial state
+    process.env = env;
+  });
+  test('While not mocking env variables, it should execute the consoleLogA function and return undefined', () => {
+    const resolvedMessage = 'It is been returned';
+    const consoleLogA = (a?: any) => console.log(`${a} by console.log`);
+    console.log = jest.fn();
+    const result = BrowserUtils.sendResponse(resolvedMessage, consoleLogA);
+    expect(result).toBeUndefined();
+    expect(console.log).toBeCalledTimes(1);
+    expect(console.log).toBeCalledWith(resolvedMessage + ' by console.log');
+  });
+});
