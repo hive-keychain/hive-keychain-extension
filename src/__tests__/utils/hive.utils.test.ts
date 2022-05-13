aa8d9031f31fc9203bbb9eda94a1b9497f39fbb0
Saturno Mangieri <saturnob612@gmail.com>
2022-05-12 22:07:12 -0400

    test

--- src/__tests__/utils/hive.utils.test.ts
+++ src/__tests__/utils/hive.utils.test.ts
@@ -14,6 +14,7 @@ describe('hive.utils tests:\n', () => {
       expect(getClientObj.address).toBeDefined();
     });
   });
+  console.log('a change');
   describe('setRpc tests:\n', () => {
     afterEach(() => {
       jest.clearAllMocks();
