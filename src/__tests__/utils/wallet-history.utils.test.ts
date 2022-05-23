bcc0c1a6ee81ebaa12feacf44bde2666d336c32b
Saturno Mangieri <saturnob612@gmail.com>
2022-05-23 14:49:11 -0400

    Tests on wallet-history.utils completed, making the PR now

--- src/__tests__/utils/wallet-history.utils.test.ts
+++ src/__tests__/utils/wallet-history.utils.test.ts
@@ -1,9 +1,14 @@
 import {
   ClaimReward,
+  CollateralizedConvert,
+  Convert,
   Delegation,
   DepositSavings,
+  FillCollateralizedConvert,
+  FillConvert,
   PowerDown,
   PowerUp,
+  ReceivedInterests,
   Transfer,
   WithdrawSavings,
 } from '@interfaces/transaction.interface';
@@ -77,7 +82,7 @@ describe('wallet-history.utils tests:\n', () => {
         true,
       );
     });
-    test('Passing valid ClaimReward but with empty rewards, will return true ', () => {
+    test('Passing valid ClaimReward but with empty rewards, will return true', () => {
       expect(
         WalletHistoryUtils.filterClaimReward(
           { hbd: '', hive: '', hp: '' } as ClaimReward,
@@ -230,9 +235,135 @@ describe('wallet-history.utils tests:\n', () => {
       expect(
         WalletHistoryUtils.filterDelegation(
           delegation,
-          'est',
+          'keychain',
           'keychain.tests',
         ),
+      ).toBe(false);
+    });
+    test('If (delegation.delegator === delegation.delegatee === activeAccountName) and trying to filter a value in delegator, will return false', () => {
+      const selfDelegation = {
+        amount: '1000.000 HIVE',
+        delegatee: 'keychain.tests',
+        delegator: 'keychain.tests',
+      } as Delegation;
+      expect(
+        WalletHistoryUtils.filterDelegation(
+          selfDelegation,
+          'keychain',
+          'keychain.tests',
+        ),
+      ).toBe(false);
+    });
+    test('If (delegation.delegator === delegation.delegatee === activeAccountName) and trying to filter a value in delegatee, will return false', () => {
+      const selfDelegation = {
+        amount: '1000.000 HIVE',
+        delegatee: 'keychain.tests',
+        delegator: 'keychain.tests',
+      } as Delegation;
+      expect(
+        WalletHistoryUtils.filterDelegation(
+          selfDelegation,
+          'key',
+          'keychain.tests',
+        ),
+      ).toBe(false);
+    });
+  });
+
+  describe('filterInterest tests:\n', () => {
+    const interest = {
+      interest: '1000.000 HBD',
+    } as ReceivedInterests;
+    test('Passing a value not contained in interest must return false', () => {
+      expect(WalletHistoryUtils.filterInterest(interest, 'HIVE')).toBe(false);
+    });
+    test('Passing a value contained in interest must return true', () => {
+      expect(WalletHistoryUtils.filterInterest(interest, 'hbd')).toBe(true);
+    });
+  });
+
+  describe('filterConversion tests:\n', () => {
+    const convert = {
+      amount: '123.005 HIVE',
+    } as Convert;
+    const collateralizedConvert = {
+      amount: '128.005 HIVE',
+    } as CollateralizedConvert;
+    test('Passing a value not contained in a Convert must return false', () => {
+      expect(WalletHistoryUtils.filterConversion(convert, 'HBD')).toBe(false);
+    });
+    test('Passing a value contained in Convert must return true', () => {
+      expect(WalletHistoryUtils.filterConversion(convert, 'hive')).toBe(true);
+    });
+    test('Passing a value not contained in a CollateralizedConvert must return false', () => {
+      expect(
+        WalletHistoryUtils.filterConversion(collateralizedConvert, 'HBD'),
+      ).toBe(false);
+    });
+    test('Passing a value contained in CollateralizedConvert must return true', () => {
+      expect(
+        WalletHistoryUtils.filterConversion(collateralizedConvert, '128.0'),
+      ).toBe(true);
+    });
+  });
+
+  describe('filterFillConversion tests:\n', () => {
+    const fillConvert = {
+      amount_in: '10.000 HIVE',
+      amount_out: '100.005 HBD',
+    } as FillConvert;
+    const fillCollateralizedConvert = {
+      amount_in: '30.001 HBD',
+      amount_out: '3009.009 HIVE',
+    } as FillCollateralizedConvert;
+    test('Passing a value contained in a fillConvert(amount_in) must return true', () => {
+      expect(
+        WalletHistoryUtils.filterFillConversion(fillConvert, '10.000'),
+      ).toBe(true);
+    });
+    test('Passing a value contained in a fillConvert(amount_out) must return true', () => {
+      expect(
+        WalletHistoryUtils.filterFillConversion(fillConvert, '100.005'),
+      ).toBe(true);
+    });
+    test('Passing a value not contained in a fillConvert(amount_in,amount_out) must return false', () => {
+      expect(WalletHistoryUtils.filterFillConversion(fillConvert, '200')).toBe(
+        false,
+      );
+    });
+    test('Passing a value contained in a fillConvert(amount_out or amount_in) must return true', () => {
+      expect(WalletHistoryUtils.filterFillConversion(fillConvert, '.')).toBe(
+        true,
+      );
+    });
+
+    test('Passing a value contained in a FillCollateralizedConvert(amount_in) must return true', () => {
+      expect(
+        WalletHistoryUtils.filterFillConversion(
+          fillCollateralizedConvert,
+          'hbd',
+        ),
+      ).toBe(true);
+    });
+    test('Passing a value contained in a FillCollateralizedConvert(amount_out) must return true', () => {
+      expect(
+        WalletHistoryUtils.filterFillConversion(
+          fillCollateralizedConvert,
+          '.009',
+        ),
+      ).toBe(true);
+    });
+    test('Passing a value not contained in a FillCollateralizedConvert(amount_in,amount_out) must return false', () => {
+      expect(
+        WalletHistoryUtils.filterFillConversion(
+          fillCollateralizedConvert,
+          'BTC',
+        ),
+      ).toBe(false);
+    });
+    test('Passing a value contained in a FillCollateralizedConvert(amount_out or amount_in) must return true', () => {
+      expect(
+        WalletHistoryUtils.filterFillConversion(fillCollateralizedConvert, 'H'),
       ).toBe(true);
     });
   });
