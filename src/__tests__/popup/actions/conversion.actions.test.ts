6ac41547b483bbb97c7584d24b0b3c3701ec6b78
Saturno Mangieri <saturnob612@gmail.com>
2022-05-30 15:42:40 -0400

    test(conversion.actions) fixed

--- src/__tests__/popup/actions/conversion.actions.test.ts
+++ src/__tests__/popup/actions/conversion.actions.test.ts
@@ -1,65 +1,39 @@
-import { ActionType } from '@popup/actions/action-type.enum';
-import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
 import * as convertionActions from 'src/popup/actions/conversion.actions';
 import HiveUtils from 'src/utils/hive.utils';
 import utilsT from 'src/__tests__/utils-for-testing/fake-data.utils';
-//configuring
-const middlewares = [thunk];
-const mockStore = configureMockStore(middlewares);
-//end configuring
+import { getFakeStore } from 'src/__tests__/utils-for-testing/fake-store';
+import { initialEmptyStateStore } from 'src/__tests__/utils-for-testing/initial-states';
 
+afterEach(() => {
+  jest.clearAllMocks();
+});
 describe('conversion.actions tests:\n', () => {
   describe('fetchConversionRequests tests:\n', () => {
-    test('Calling this action will return convertions from this user and a FETCH_CONVERSION_REQUESTS action', async () => {
+    test('Must set conversions', async () => {
       const fakeArrayResponse = [
         utilsT.fakeHbdConversionsResponse,
         utilsT.fakeHiveConversionsResponse,
       ];
-      const mockGetConvertionRequests = (HiveUtils.getConversionRequests = jest
+      HiveUtils.getConversionRequests = jest
         .fn()
-        .mockResolvedValueOnce(fakeArrayResponse));
-      const mockedStore = mockStore({});
-      return await mockedStore
-        .dispatch<any>(
-          convertionActions.fetchConversionRequests(utilsT.userData.username),
-        )
-        .then(() => {
-          expect(mockedStore.getActions()).toEqual([
-            {
-              type: ActionType.FETCH_CONVERSION_REQUESTS,
-              payload: fakeArrayResponse,
-            },
-          ]);
-          expect(mockGetConvertionRequests).toBeCalledTimes(1);
-          expect(mockGetConvertionRequests).toBeCalledWith(
-            utilsT.userData.username,
-          );
-          mockGetConvertionRequests.mockReset();
-          mockGetConvertionRequests.mockRestore();
-        });
+        .mockResolvedValueOnce(fakeArrayResponse);
+      const fakeStore = getFakeStore(initialEmptyStateStore);
+      await fakeStore.dispatch<any>(
+        convertionActions.fetchConversionRequests('wesp05'),
+      );
+      expect(fakeStore.getState().conversions).toEqual(fakeArrayResponse);
     });
     test('If an error occurs on the request, will thrown a unhandled error', async () => {
       const errorRequest = new Error('Custom Error');
-      const mockGetConvertionRequests = (HiveUtils.getConversionRequests = jest
+      HiveUtils.getConversionRequests = jest
         .fn()
-        .mockRejectedValueOnce(errorRequest));
-      const mockedStore = mockStore({});
+        .mockRejectedValueOnce(errorRequest);
       try {
-        return await mockedStore
-          .dispatch<any>(
-            convertionActions.fetchConversionRequests(utilsT.userData.username),
-          )
-          .then(() => {
-            expect(mockedStore.getActions()).toEqual([
-              {
-                type: ActionType.FETCH_CONVERSION_REQUESTS,
-                payload: '',
-              },
-            ]);
-            mockGetConvertionRequests.mockReset();
-            mockGetConvertionRequests.mockRestore();
-          });
+        const fakeStore = getFakeStore(initialEmptyStateStore);
+        await fakeStore.dispatch<any>(
+          convertionActions.fetchConversionRequests('wesp05'),
+        );
+        expect(fakeStore.getState().conversions).toEqual(null);
       } catch (error) {
         expect(error).toEqual(errorRequest);
       }
