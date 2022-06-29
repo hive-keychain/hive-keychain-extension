2fd3544670de1329eecc3e0d036daecd909a4f12
Cedric Gui <cedric.guillas78@gmail.com>
2022-06-29 16:14:24 +0800

    feat(auto claim): Added auto claim savings interests

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,9 @@ const Config = {
       MIN_RC_PCT: +(process.env.DEV_CLAIM_ACCOUNT_RC_PCT || 85),
       MIN_RC: +(process.env.DEV_CLAIM_ACCOUNT_MIN_RC || 1.2 * 10 * 10 ** 12), // 20% more than 10^13 (current creation cost)
     },
+    savings: {
+      delay: +(process.env.DEV_CLAIM_SAVINGS_DELAY || 30),
+    },
   },
   PROPOSAL: 216,
   MIN_LOADING_TIME: 1000,
