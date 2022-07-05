49de4e5dfd53aaa8dca016b9378d9c0a9b51cc19
Cedric Gui <cedric.guillas78@gmail.com>
2022-07-05 11:51:13 +0800

    removed logs

--- src/popup/store/index.ts
+++ src/popup/store/index.ts
@@ -27,7 +27,6 @@ let previousHiveEngineConfig = store.getState().hiveEngineConfig;
 store.subscribe(() => {
   const { accounts, mk, activeRpc, activeAccount, hiveEngineConfig } =
     store.getState();
-  console.log(hiveEngineConfig);
   if (!AccountUtils.isAccountListIdentical(previousAccounts, accounts)) {
     previousAccounts = accounts;
     AccountUtils.saveAccounts(accounts, mk);
