4c28613f58a5c2e1463f53385a416772c0197e76
Saturno Mangieri <saturnob612@gmail.com>
2022-07-29 13:36:34 -0400

    tests settings section, In progress

---
+++ src/__tests__/popup/pages/app-container/settings/advanced-settings/auto-lock/auto-lock.component.test.tsx
@@ -0,0 +1,7 @@
+import config from 'src/__tests__/utils-for-testing/setups/config';
+config.byDefault();
+describe('auto-lock.component tests:\n', () => {
+  it.todo('Must load autolock and match snapshot');
+  it.todo('Must load stored autolock value and match snapshot');
+  it.todo('Must set autolock, show message and goback to menu');
+});
