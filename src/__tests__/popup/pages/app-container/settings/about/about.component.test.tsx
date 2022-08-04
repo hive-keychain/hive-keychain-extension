bba89979321da12845794622846bbeaa445d880d
Saturno Mangieri <saturnob612@gmail.com>
2022-08-04 10:50:29 -0400

    tests(settings-main-page) completed + fixes

--- src/__tests__/popup/pages/app-container/settings/about/about.component.test.tsx
+++ src/__tests__/popup/pages/app-container/settings/about/about.component.test.tsx
@@ -1,7 +1,7 @@
 import App from '@popup/App';
 import { screen } from '@testing-library/react';
 import React from 'react';
-import settings from 'src/__tests__/popup/pages/app-container/settings/mocks/settings';
+import settingsMainPage from 'src/__tests__/popup/pages/app-container/settings/settings-main-page/mocks/settings-main-page';
 import alButton from 'src/__tests__/utils-for-testing/aria-labels/al-button';
 import alDiv from 'src/__tests__/utils-for-testing/aria-labels/al-div';
 import manipulateStrings from 'src/__tests__/utils-for-testing/helpers/manipulate-strings';
@@ -9,14 +9,14 @@ import config from 'src/__tests__/utils-for-testing/setups/config';
 import { clickAwait } from 'src/__tests__/utils-for-testing/setups/events';
 config.byDefault();
 describe('about.component tests:\n', () => {
-  settings.methods.afterEach;
+  settingsMainPage.methods.afterEach;
   beforeEach(async () => {
-    await settings.beforeEach(<App />);
+    await settingsMainPage.beforeEach(<App />);
     await clickAwait([alButton.menuPreFix + 'info']);
   });
   it('Must show about message', () => {
     let cleaned = manipulateStrings.removeHtmlTags(
-      settings.constants.messages.about,
+      settingsMainPage.constants.messages.about,
     );
     expect(screen.getByLabelText(alDiv.about.content)).toHaveTextContent(
       cleaned,
