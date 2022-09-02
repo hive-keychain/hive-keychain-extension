59d4e1ac3f663ad19b5a57a88e9084ef84a5c5f4
Saturno Mangieri <saturnob612@gmail.com>
2022-09-02 06:24:21 -0400

    tests(fix missingKey.logic cov 100)

--- src/__tests__/background/requests/logic/missingKey.logic.test.ts
+++ src/__tests__/background/requests/logic/missingKey.logic.test.ts
@@ -4,11 +4,13 @@ describe('missingKey.logic tests:\n', () => {
   const { methods, constants, spies, callback } = missingKeyLogic;
   const { request, tab, requestHandler, username } = constants;
   const { typeWif } = constants;
+  methods.beforeEach;
   methods.afterEach;
   it('Must call createPopup with sendErrors callback', () => {
     missingKey(requestHandler, tab, request, username, typeWif);
-    expect(spies.createPopup.mock.calls[0][0].toString()).toEqual(
-      callback.sendErrors.toString(),
+    expect(JSON.stringify(spies.createPopup.mock.calls[0][0])).toEqual(
+      JSON.stringify(callback.sendErrors),
     );
+    expect(spies.createPopup.mock.calls[0][1]).toEqual(requestHandler);
   });
 });
--- src/__tests__/background/requests/logic/mocks/missingKey.logic.ts
+++ src/__tests__/background/requests/logic/mocks/missingKey.logic.ts
@@ -1,9 +1,9 @@
 import { RequestsHandler } from '@background/requests';
 import sendErrors from '@background/requests/errors';
 import * as dialogLifeCycle from 'src/background/requests/dialog-lifecycle';
+import * as SendErrorsModule from 'src/background/requests/errors';
 import keychainRequest from 'src/__tests__/utils-for-testing/data/keychain-request';
 import mk from 'src/__tests__/utils-for-testing/data/mk';
-import manipulateStrings from 'src/__tests__/utils-for-testing/helpers/manipulate-strings';
 
 const request = keychainRequest.noValues.decode;
 const tab = 0;
@@ -12,7 +12,19 @@ const username = mk.user.one;
 const typeWif = 'type_wif';
 
 const spies = {
-  createPopup: jest.spyOn(dialogLifeCycle, 'createPopup'),
+  createPopup: jest
+    .spyOn(dialogLifeCycle, 'createPopup')
+    .mockImplementation(
+      (
+        callback: () => void,
+        requestHandler: RequestsHandler,
+        popupHtml = 'dialog.html',
+      ) => {
+        jest.spyOn(SendErrorsModule, 'default');
+        chrome.runtime.sendMessage = jest.fn();
+        callback();
+      },
+    ),
 };
 
 const callback = {
@@ -29,10 +41,12 @@ const callback = {
 };
 
 const methods = {
+  beforeEach: beforeEach(() => {
+    chrome.i18n.getUILanguage = jest.fn().mockReturnValue('en-US');
+  }),
   afterEach: afterEach(() => {
     jest.clearAllMocks();
   }),
-  clean: (str: string) => manipulateStrings.removeTabs(str),
 };
 
 const constants = {
