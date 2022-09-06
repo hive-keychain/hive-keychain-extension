88b169391a24c3794a2116a6e23c64621af828b6
Saturno Mangieri <saturnob612@gmail.com>
2022-09-06 10:19:28 -0400

    tests(ignored fileUtils)

--- src/utils/file.utils.ts
+++ src/utils/file.utils.ts
@@ -11,9 +11,9 @@ const toBase64 = (file: File): Promise<string> =>
     };
     reader.onerror = (error) => reject(error);
   });
-
+/* istanbul ignore next */
 const FileUtils = {
   toBase64,
 };
-
+/* istanbul ignore next */
 export default FileUtils;
