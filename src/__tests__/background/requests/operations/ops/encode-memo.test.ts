ec0fcfce65ec1e0d6408ae0e717a1a015f8c4ac6
theghost1980 <saturnob612@gmail.com>
2022-10-31 13:50:46 -0400

    tests(fix: bg/encode-memo error cases)

--- src/__tests__/background/requests/operations/ops/encode-memo.test.ts
+++ src/__tests__/background/requests/operations/ops/encode-memo.test.ts
@@ -1,11 +1,11 @@
 import { encodeMessage } from '@background/requests/operations/ops/encode-memo';
 import { KeychainKeyTypes } from '@interfaces/keychain.interface';
-import { AssertionError } from 'assert';
 import encodeMemo from 'src/__tests__/background/requests/operations/ops/mocks/encode-memo';
 import messages from 'src/__tests__/background/requests/operations/ops/mocks/messages';
 import accounts from 'src/__tests__/utils-for-testing/data/accounts';
 import memo from 'src/__tests__/utils-for-testing/data/memo';
 import userData from 'src/__tests__/utils-for-testing/data/user-data';
+import { ResultOperation } from 'src/__tests__/utils-for-testing/interfaces/assertions';
 describe('encode-memo tests:\n', () => {
   const { methods, constants, mocks, spies } = encodeMemo;
   const { requestHandler, data } = constants;
@@ -14,41 +14,27 @@ describe('encode-memo tests:\n', () => {
   it('Must return error if no key on handler', async () => {
     mocks.client.database.getAccounts([accounts.extended]);
     data.message = memo._default.decoded;
-    const result = await encodeMessage(requestHandler, data);
-    const { request_id, ...datas } = data;
-    const errorTitle =
-      "Cannot read properties of undefined (reading 'memo_key')";
-    expect(result).toEqual(
-      messages.error.answerError(
-        new AssertionError({
-          expected: true,
-          operator: '==',
-          message: errorTitle,
-        }),
-        datas,
-        request_id,
-        chrome.i18n.getMessage('bgd_ops_encode_err'),
-        null,
-      ),
-    );
+    const resultOperation = (await encodeMessage(
+      requestHandler,
+      data,
+    )) as ResultOperation;
+    const { success, result, error, ...datas } = resultOperation.msg;
+    expect(success).toBe(false);
+    expect(result).toBeNull();
+    expect((error as TypeError).message).toContain('private_key');
   });
   it('Must return error if receiver not received', async () => {
     mocks.client.database.getAccounts([]);
     requestHandler.data.key = userData.one.nonEncryptKeys.memo;
     data.message = memo._default.decoded;
-    const result = await encodeMessage(requestHandler, data);
-    const { request_id, ...datas } = data;
-    const errorTitle =
-      "Cannot read properties of undefined (reading 'memo_key')";
-    expect(result).toEqual(
-      messages.error.answerError(
-        new TypeError(errorTitle),
-        datas,
-        request_id,
-        chrome.i18n.getMessage('bgd_ops_encode_err'),
-        null,
-      ),
-    );
+    const resultOperation = (await encodeMessage(
+      requestHandler,
+      data,
+    )) as ResultOperation;
+    const { success, result, error, ...datas } = resultOperation.msg;
+    expect(success).toBe(false);
+    expect(result).toBeNull();
+    expect((error as TypeError).message).toContain('memo_key');
   });
   it('Must use memo_key if method as memo', async () => {
     mocks.client.database.getAccounts([accounts.extended]);
