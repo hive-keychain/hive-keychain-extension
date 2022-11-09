a1e9b148a02261a207f1be88d4753763e8ab0934
Cedric Gui <cedric.guillas78@gmail.com>
2022-11-09 13:28:06 +0800

    fix config file

--- src/config.ts
+++ src/config.ts
@@ -28,7 +28,7 @@ const Config = {
       process.env.TRANSAK_DEV_API_KEY || '716078e4-939c-445a-8c6d-534614cd31b1',
   },
   loader: {
-    minDuration: +(process.env.TEST_LOADER_MIN_DURATION || 1000),
+    minDuration: process.env.NODE_ENV ? 0 : 1000,
   },
 };
 
