baf1b1bfcc3a1d0fd664cdc010aabf77e2130e0d
theghost1980 <saturnob612@gmail.com>
2023-01-04 10:02:37 -0400

    fix ops/custom-json tests

--- src/__tests__/background/requests/operations/ops/custom-json.test.ts
+++ src/__tests__/background/requests/operations/ops/custom-json.test.ts
@@ -1,5 +1,6 @@
 import { broadcastCustomJson } from '@background/requests/operations/ops/custom-json';
 import { KeychainKeyTypesLC } from '@interfaces/keychain.interface';
+import { HiveTxUtils } from 'src/utils/hive-tx.utils';
 import customJsonMocks from 'src/__tests__/background/requests/operations/ops/mocks/custom-json-mocks';
 import messages from 'src/__tests__/background/requests/operations/ops/mocks/messages';
 import userData from 'src/__tests__/utils-for-testing/data/user-data';
@@ -9,7 +10,6 @@ describe('custom-json tests:\n', () => {
   methods.afterEach;
   methods.beforeEach;
   it('Must call getUserKey if no key on handler', async () => {
-    mocks.client.broadcast.json(confirmed);
     await broadcastCustomJson(requestHandler, data);
     expect(spies.getUserKey).toBeCalledWith(
       data.username!,
@@ -17,17 +17,20 @@ describe('custom-json tests:\n', () => {
     );
   });
   it('Must return success', async () => {
-    mocks.client.broadcast.json(confirmed);
+    const mHiveTxSendOp = jest
+      .spyOn(HiveTxUtils, 'sendOperation')
+      .mockResolvedValueOnce(true);
     requestHandler.data.key = userData.one.nonEncryptKeys.active;
     const result = await broadcastCustomJson(requestHandler, data);
     const { request_id, ...datas } = data;
     expect(result).toEqual(
       messages.success.broadcast(
-        confirmed,
+        true,
         datas,
         request_id,
         chrome.i18n.getMessage('bgd_ops_broadcast'),
       ),
     );
+    mHiveTxSendOp.mockRestore();
   });
 });
