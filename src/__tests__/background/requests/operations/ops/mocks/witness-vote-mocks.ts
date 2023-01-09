e88992d10fde455596c16b21eadcf228395758b1
theghost1980 <saturnob612@gmail.com>
2023-01-09 06:45:59 -0400

    fix: /ops/witness-vote.test

--- src/__tests__/background/requests/operations/ops/mocks/witness-vote-mocks.ts
+++ src/__tests__/background/requests/operations/ops/mocks/witness-vote-mocks.ts
@@ -62,7 +62,7 @@ const methods = {
           error,
           datas,
           request_id,
-          `${chrome.i18n.getMessage('bgd_ops_error')} : ${errorMessage}`,
+          errorMessage,
           undefined,
         ),
       );
@@ -71,7 +71,7 @@ const methods = {
       const { request_id, ...datas } = data;
       expect(result).toEqual(
         messages.success.answerSucess(
-          confirmed,
+          true,
           datas,
           request_id,
           message,
--- src/__tests__/background/requests/operations/ops/witness-vote.test.ts
+++ src/__tests__/background/requests/operations/ops/witness-vote.test.ts
@@ -1,5 +1,6 @@
 import { broadcastWitnessVote } from '@background/requests/operations/ops/witness-vote';
 import { KeychainKeyTypesLC } from '@interfaces/keychain.interface';
+import { HiveTxUtils } from 'src/utils/hive-tx.utils';
 import witnessVoteMocks from 'src/__tests__/background/requests/operations/ops/mocks/witness-vote-mocks';
 import userData from 'src/__tests__/utils-for-testing/data/user-data';
 describe('witness-vote tests:\n', () => {
@@ -15,19 +16,26 @@ describe('witness-vote tests:\n', () => {
     );
   });
   it('Must return error if no key on handler', async () => {
-    const error = 'private key should be a Buffer';
+    const error = "Cannot read properties of undefined (reading 'toString')";
     const result = await broadcastWitnessVote(requestHandler, data);
     methods.assert.error(result, new TypeError(error), data, error);
   });
   it('Must return success when vote', async () => {
+    const mHiveTxSendOp = jest
+      .spyOn(HiveTxUtils, 'sendOperation')
+      .mockResolvedValue(true);
     requestHandler.data.key = userData.one.nonEncryptKeys.active;
     const result = await broadcastWitnessVote(requestHandler, data);
     methods.assert.success(
       result,
       chrome.i18n.getMessage('bgd_ops_witness_voted', [data.witness]),
     );
+    mHiveTxSendOp.mockRestore();
   });
   it('Must return success when unvote', async () => {
+    const mHiveTxSendOp = jest
+      .spyOn(HiveTxUtils, 'sendOperation')
+      .mockResolvedValue(true);
     requestHandler.data.key = userData.one.nonEncryptKeys.active;
     data.vote = false;
     const result = await broadcastWitnessVote(requestHandler, data);
@@ -35,5 +43,6 @@ describe('witness-vote tests:\n', () => {
       result,
       chrome.i18n.getMessage('bgd_ops_witness_unvoted', [data.witness]),
     );
+    mHiveTxSendOp.mockRestore();
   });
 });
