656efcc77c3534c94930160f9371e3c2dd8a919d
theghost1980 <saturnob612@gmail.com>
2023-01-18 09:58:41 -0400

    tests: create-claimed-account.test updated

--- src/__tests__/background/requests/operations/ops/create-claimed-account.test.ts
+++ src/__tests__/background/requests/operations/ops/create-claimed-account.test.ts
@@ -8,37 +8,62 @@ describe('create-claimed-account tests:\n', () => {
   const { requestHandler, data, confirmed } = constants;
   methods.afterEach;
   methods.beforeEach;
-  it('Must return error if no key on handler', async () => {
-    const message = "Cannot read properties of undefined (reading 'toString')";
-    const result = await broadcastCreateClaimedAccount(requestHandler, data);
-    const { request_id, ...datas } = data;
-    expect(result).toEqual(
-      messages.error.keyBuffer(
-        datas,
-        request_id,
-        new TypeError(message),
-        message,
-      ),
-    );
+  describe('Default cases:\n', () => {
+    it('Must return error if no key on handler', async () => {
+      const message =
+        "Cannot read properties of undefined (reading 'toString')";
+      const result = await broadcastCreateClaimedAccount(requestHandler, data);
+      const { request_id, ...datas } = data;
+      expect(result).toEqual(
+        messages.error.keyBuffer(
+          datas,
+          request_id,
+          new TypeError(message),
+          message,
+        ),
+      );
+    });
+    it('Must return success on claimed account', async () => {
+      const mHiveTxSendOp = jest
+        .spyOn(HiveTxUtils, 'sendOperation')
+        .mockResolvedValueOnce(true);
+      requestHandler.setKeys(
+        userData.one.nonEncryptKeys.active,
+        userData.one.encryptKeys.active,
+      );
+      const result = await broadcastCreateClaimedAccount(requestHandler, data);
+      const { request_id, ...datas } = data;
+      expect(result).toEqual(
+        messages.success.broadcast(
+          false,
+          datas,
+          request_id,
+          chrome.i18n.getMessage('bgd_ops_create_account', [data.new_account]),
+        ),
+      );
+      mHiveTxSendOp.mockRestore();
+    });
   });
-  it('Must return success on claimed account', async () => {
-    const mHiveTxSendOp = jest
-      .spyOn(HiveTxUtils, 'sendOperation')
-      .mockResolvedValueOnce(true);
-    requestHandler.setKeys(
-      userData.one.nonEncryptKeys.active,
-      userData.one.encryptKeys.active,
-    );
-    const result = await broadcastCreateClaimedAccount(requestHandler, data);
-    const { request_id, ...datas } = data;
-    expect(result).toEqual(
-      messages.success.broadcast(
-        false,
-        datas,
-        request_id,
-        chrome.i18n.getMessage('bgd_ops_create_account', [data.new_account]),
-      ),
-    );
-    mHiveTxSendOp.mockRestore();
+
+  describe('Using ledger cases:\n', () => {
+    it('Must return success on claimed account', async () => {
+      mocks.HiveTxUtils.sendOperation(true);
+      mocks.broadcastAndConfirmTransactionWithSignature(true);
+      mocks.LedgerModule.getSignatureFromLedger('signed!');
+      requestHandler.setKeys(
+        '#LedgerKeyHere1234',
+        userData.one.encryptKeys.active,
+      );
+      const result = await broadcastCreateClaimedAccount(requestHandler, data);
+      const { request_id, ...datas } = data;
+      expect(result).toEqual(
+        messages.success.broadcast(
+          true,
+          datas,
+          request_id,
+          chrome.i18n.getMessage('bgd_ops_create_account', [data.new_account]),
+        ),
+      );
+    });
   });
 });
--- src/__tests__/background/requests/operations/ops/mocks/create-claimed-account.ts
+++ src/__tests__/background/requests/operations/ops/mocks/create-claimed-account.ts
@@ -1,3 +1,4 @@
+import LedgerModule from '@background/ledger.module';
 import { RequestsHandler } from '@background/requests/request-handler';
 import { AuthorityType, TransactionConfirmation } from '@hiveio/dhive';
 import {
@@ -5,6 +6,7 @@ import {
   RequestCreateClaimedAccount,
   RequestId,
 } from '@interfaces/keychain.interface';
+import { HiveTxUtils } from 'src/utils/hive-tx.utils';
 import mk from 'src/__tests__/utils-for-testing/data/mk';
 import mocksImplementation from 'src/__tests__/utils-for-testing/implementations/implementations';
 
@@ -41,6 +43,20 @@ const mocks = {
     (chrome.i18n.getMessage = jest
       .fn()
       .mockImplementation(mocksImplementation.i18nGetMessageCustom)),
+  broadcastAndConfirmTransactionWithSignature: (result: boolean) =>
+    jest
+      .spyOn(HiveTxUtils, 'broadcastAndConfirmTransactionWithSignature')
+      .mockResolvedValue(result),
+  LedgerModule: {
+    getSignatureFromLedger: (signature: string) =>
+      jest
+        .spyOn(LedgerModule, 'getSignatureFromLedger')
+        .mockResolvedValue(signature),
+  },
+  HiveTxUtils: {
+    sendOperation: (result: boolean) =>
+      jest.spyOn(HiveTxUtils, 'sendOperation').mockResolvedValue(result),
+  },
 };
 
 const methods = {
