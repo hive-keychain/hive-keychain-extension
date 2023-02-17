4419e002bfdcd1b43c9b81e8dadf0be7892fff4c
stoodkev <quentin.correa@gmail.com>
2023-02-17 18:08:50 +0800

    fix errors

--- src/dialog/pages/sign-transaction.tsx
+++ src/dialog/pages/sign-transaction.tsx
@@ -1,7 +1,6 @@
 import HiveLedgerApp from '@engrave/ledger-app-hive';
 import { Transaction } from '@hiveio/dhive';
 import { Key } from '@interfaces/keys.interface';
-import { LoadingOperation } from '@popup/reducers/loading.reducer';
 import { DialogCommand } from '@reference-data/dialog-message-key.enum';
 import React, { useEffect, useState } from 'react';
 import { LoadingComponent } from 'src/common-ui/loading/loading.component';
@@ -21,20 +20,15 @@ export type SignFromLedgerRequestMessage = {
 };
 
 const SignTransaction = (props: Props) => {
-  const [loadingOperations, setLoadingOperations] = useState<
-    LoadingOperation[]
-  >([{ name: 'html_popup_waiting_for_ledger_confirmation', done: false }]);
+  const [loadingOperations, setLoadingOperations] = useState([
+    { name: 'html_popup_waiting_for_ledger_confirmation', done: false },
+  ]);
 
   useEffect(() => {
     signTransaction(props.data);
   }, []);
 
   const signTransaction = async (data: SignFromLedgerRequestMessage) => {
-    const operations: LoadingOperation[] = [
-      { name: 'html_popup_waiting_for_ledger_confirmation', done: false },
-      { name: 'html_popup_broadcasting_transaction', done: false },
-    ];
-
     try {
       let signature;
       if (!HiveLedgerApp.isDisplayableOnDevice(data.transaction)) {
@@ -42,35 +36,25 @@ const SignTransaction = (props: Props) => {
         try {
           hashSignPolicy = (await LedgerUtils.getSettings()).hashSignPolicy;
         } catch (err: any) {
-          throw ErrorUtils.parse(err);
+          throw ErrorUtils.parseLedger(err);
         }
 
         if (!hashSignPolicy) {
           throw new KeychainError('error_ledger_no_hash_sign_policy');
         }
         const digest = HiveLedgerApp.getTransactionDigest(data.transaction);
-        const splitedDigest: string[] = digest.match(/.{1,17}/g) as string[];
-        operations.unshift({
-          name: 'ledger_signing_hash',
-          operationParams: [splitedDigest.join('<br />')],
-          done: false,
-          hideDots: true,
-        });
-        setLoadingOperations(operations);
         signature = await LedgerUtils.signHash(digest, data.key!);
-        operations[1].done = true;
       } else {
-        setLoadingOperations(operations);
         const signedTransaction = await LedgerUtils.signTransaction(
           data.transaction,
           data.key!,
         );
         signature = signedTransaction.signatures[0];
-        operations[0].done = true;
       }
-
-      setLoadingOperations(operations);
-
+      setLoadingOperations([
+        { name: 'html_popup_waiting_for_ledger_confirmation', done: true },
+        { name: 'html_popup_broadcasting_transaction', done: false },
+      ]);
       chrome.runtime.sendMessage({
         command: DialogCommand.RETURN_SIGNATURE,
         signature: signature,
