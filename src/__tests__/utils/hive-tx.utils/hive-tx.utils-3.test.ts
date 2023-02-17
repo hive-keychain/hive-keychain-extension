4d4f833e9b7a1e034d6261647a27ecbba67d7d18
theghost1980 <saturnob612@gmail.com>
2023-02-17 07:37:22 -0400

    fixed: hive-tx.utils-3.test.ts

--- src/__tests__/utils/hive-tx.utils/hive-tx.utils-3.test.ts
+++ src/__tests__/utils/hive-tx.utils/hive-tx.utils-3.test.ts
@@ -21,9 +21,7 @@ describe('hive-tx.utils.test.ts part 3', () => {
         try {
           await HiveTxUtils.signTransaction(constants.tx, '#1qw23eer4e');
         } catch (error) {
-          expect(error).toEqual(
-            new KeychainError('error_ledger_no_hash_sign_policy'),
-          );
+          expect(error).toEqual(new KeychainError('html_ledger_not_supported'));
         }
       });
 
