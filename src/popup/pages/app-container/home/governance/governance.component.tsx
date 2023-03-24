413da5958c4e269cf7c8026095b3af0d15436e73
theghost1980 <saturnob612@gmail.com>
2023-03-24 10:11:42 -0400

    work in progress, SR

--- src/popup/pages/app-container/home/governance/governance.component.tsx
+++ src/popup/pages/app-container/home/governance/governance.component.tsx
@@ -1,15 +1,38 @@
 import { setTitleContainerProperties } from '@popup/actions/title-container.actions';
 import { ProposalTabComponent } from '@popup/pages/app-container/home/governance/proposal-tab/proposal-tab.component';
 import { ProxyTabComponent } from '@popup/pages/app-container/home/governance/proxy-tab/proxy-tab.component';
+import { WitnessPageTabComponent } from '@popup/pages/app-container/home/governance/witness-page-tab/witness-page-tab.component';
 import { WitnessTabComponent } from '@popup/pages/app-container/home/governance/witness-tab/witness-tab.component';
 import { RootState } from '@popup/store';
 import React, { useEffect } from 'react';
 import { connect, ConnectedProps } from 'react-redux';
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 import 'react-tabs/style/react-tabs.scss';
+import { HiveTxUtils } from 'src/utils/hive-tx.utils';
 import './governance.component.scss';
 
-const Governance = ({ setTitleContainerProperties }: PropsFromRedux) => {
+const Governance = ({
+  setTitleContainerProperties,
+  activeAccount,
+}: PropsFromRedux) => {
+  //testing part hook //TODO to remove or clean up
+  useEffect(() => {
+    checkIfWitnessAccount('stoodkev'); //TODO later to be activeAccount.name!
+    //the idea is checking if isWitness, so the page will be displayed.
+  });
+
+  const checkIfWitnessAccount = async (accountName: string) => {
+    const witnessAccount = await HiveTxUtils.getData(
+      'condenser_api.get_witness_by_account',
+      [accountName],
+    );
+    console.log({ witnessAccount });
+    // await HiveTxUtils.getData('condenser_api.get_witness_by_account', [
+    //   username,
+    // ]);
+  };
+  //end testing part
+
   useEffect(() => {
     setTitleContainerProperties({
       title: 'popup_html_governance',
@@ -24,6 +47,8 @@ const Governance = ({ setTitleContainerProperties }: PropsFromRedux) => {
           <Tab>{chrome.i18n.getMessage('popup_html_witness')}</Tab>
           <Tab>{chrome.i18n.getMessage('popup_html_proxy')}</Tab>
           <Tab>{chrome.i18n.getMessage('popup_html_proposal')}</Tab>
+          {/* //TODO add bellow to locales */}
+          <Tab>{chrome.i18n.getMessage('popup_html_witness_page')}</Tab>
         </TabList>
         <TabPanel>
           <WitnessTabComponent></WitnessTabComponent>
@@ -33,6 +58,9 @@ const Governance = ({ setTitleContainerProperties }: PropsFromRedux) => {
         </TabPanel>
         <TabPanel>
           <ProposalTabComponent></ProposalTabComponent>
+        </TabPanel>
+        <TabPanel>
+          <WitnessPageTabComponent></WitnessPageTabComponent>
         </TabPanel>
       </Tabs>
     </div>
