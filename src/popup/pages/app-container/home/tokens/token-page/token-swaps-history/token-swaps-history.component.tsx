30e81c857b22d20144f9cb212f4c4fcdedf8e498
Cedric Gui <cedric.guillas78@gmail.com>
2023-04-27 15:06:50 +0800

    feat(swap history): Updated empty history page

--- src/popup/pages/app-container/home/tokens/token-page/token-swaps-history/token-swaps-history.component.tsx
+++ src/popup/pages/app-container/home/tokens/token-page/token-swaps-history/token-swaps-history.component.tsx
@@ -1,11 +1,13 @@
 import { Swap } from '@interfaces/swap-token.interface';
 import { setInfoMessage } from '@popup/actions/message.actions';
 import { setTitleContainerProperties } from '@popup/actions/title-container.actions';
+import { Icons } from '@popup/icons.enum';
 import { TokenSwapsHistoryItemComponent } from '@popup/pages/app-container/home/tokens/token-page/token-swaps-history/token-swaps-history-item/token-swaps-history-item.component';
 import { RootState } from '@popup/store';
 import React, { useEffect, useState } from 'react';
 import { ConnectedProps, connect } from 'react-redux';
 import 'react-tabs/style/react-tabs.scss';
+import Icon from 'src/common-ui/icon/icon.component';
 import Config from 'src/config';
 import { SwapTokenUtils } from 'src/utils/swap-token.utils';
 import './token-swaps-history.component.scss';
@@ -50,6 +52,10 @@ const TokenSwapsHistory = ({
   }, [autoRefreshCountdown]);
 
   const initSwapHistory = async () => {
+    await refresh();
+  };
+
+  const refresh = async () => {
     setLoading(true);
     const result = await SwapTokenUtils.retrieveSwapHistory(
       activeAccount.name!,
@@ -62,19 +68,28 @@ const TokenSwapsHistory = ({
   return (
     <div className="token-swaps-history">
       <div className="refresh-panel">
-        {autoRefreshCountdown && (
-          <span>
+        {!!autoRefreshCountdown && (
+          <>
             {chrome.i18n.getMessage('swap_refresh_countdown', [
               autoRefreshCountdown?.toString(),
             ])}
-          </span>
+            <Icon name={Icons.REFRESH} onClick={refresh} rotate={loading} />
+          </>
         )}
+        {autoRefreshCountdown && <span></span>}
       </div>
-      {history.map((item, index) => {
-        return (
-          <TokenSwapsHistoryItemComponent key={`item-${index}`} swap={item} />
-        );
-      })}
+      {history.length > 0 &&
+        history.map((item, index) => {
+          return (
+            <TokenSwapsHistoryItemComponent key={`item-${index}`} swap={item} />
+          );
+        })}
+      {history.length === 0 && (
+        <div className="empty-history-panel">
+          <Icon name={Icons.INBOX} />
+          <span>{chrome.i18n.getMessage('swap_no_history')}</span>
+        </div>
+      )}
     </div>
   );
 };
