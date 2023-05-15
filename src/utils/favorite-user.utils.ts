ac7955ed2c3f48015ab978466c4396e852963a55
Cedric Gui <cedric.guillas78@gmail.com>
2023-05-15 09:37:03 +0800

    refactor(favorite utils): Moved interfaces

--- src/utils/favorite-user.utils.ts
+++ src/utils/favorite-user.utils.ts
@@ -5,7 +5,10 @@ import {
   AutoCompleteValue,
   AutoCompleteValues,
 } from '@interfaces/autocomplete.interface';
-import { FavoriteUserItems } from '@interfaces/favorite-user.interface';
+import {
+  FavoriteUserItems,
+  FavoriteUserListName,
+} from '@interfaces/favorite-user.interface';
 import { LocalAccount } from '@interfaces/local-account.interface';
 import { exchanges } from '@popup/pages/app-container/home/buy-coins/buy-coins-list-item.list';
 import { LocalStorageKeyEnum } from '@reference-data/local-storage-key.enum';
@@ -76,9 +79,9 @@ const saveFavoriteUser = async (
     favoriteUser[activeAccount.name!] = [];
   }
 
-  let found = undefined;
+  let found = false;
   if (favoriteUser[activeAccount.name!].length > 0) {
-    found = Object.values(favoriteUser[activeAccount.name!]).find(
+    found = Object.values(favoriteUser[activeAccount.name!]).some(
       (favValue: any) => favValue.value === username,
     );
   }
@@ -99,20 +102,6 @@ const saveFavoriteUser = async (
   );
 };
 
-export enum FavoriteUserListName {
-  USERS = 'users',
-  LOCAL_ACCOUNTS = 'local_accounts',
-  EXCHANGES = 'exchanges',
-}
-export interface FavoriteUserList {
-  name: FavoriteUserListName;
-  list: AutoCompleteValue[];
-}
-export interface FavoriteAccounts {
-  account: string;
-  label: string;
-  subLabel?: string;
-}
 const getAutocompleteListByCategories = async (
   username: string,
   localAccounts: LocalAccount[],
@@ -153,6 +142,7 @@ const getAutocompleteListByCategories = async (
         favoriteUsersList.values.push(fav);
     }
   }
+
   for (const localAccount of localAccounts) {
     if (localAccount.name !== username) {
       favoriteLocalAccountsList.values.push({
