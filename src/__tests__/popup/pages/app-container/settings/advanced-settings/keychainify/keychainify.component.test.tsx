9903d0b6257c3318ca33f73f67a2facda111b0b4
theghost1980 <saturnob612@gmail.com>
2023-05-17 13:22:46 -0400

    keychainify.component.test: removed /mocks, refactored

--- src/__tests__/popup/pages/app-container/settings/advanced-settings/keychainify/keychainify.component.test.tsx
+++ src/__tests__/popup/pages/app-container/settings/advanced-settings/keychainify/keychainify.component.test.tsx
@@ -1,29 +1,70 @@
-import { waitFor } from '@testing-library/react';
-import keychainify from 'src/__tests__/popup/pages/app-container/settings/advanced-settings/keychainify/mocks/keychainify';
-import alCheckbox from 'src/__tests__/utils-for-testing/aria-labels/al-checkbox';
-import alComponent from 'src/__tests__/utils-for-testing/aria-labels/al-component';
-import assertion from 'src/__tests__/utils-for-testing/preset/assertion';
-import config from 'src/__tests__/utils-for-testing/setups/config';
-import { clickAwait } from 'src/__tests__/utils-for-testing/setups/events';
-config.byDefault();
+import App from '@popup/App';
+import { Icons } from '@popup/icons.enum';
+import { LocalStorageKeyEnum } from '@reference-data/local-storage-key.enum';
+import { Screen } from '@reference-data/screen.enum';
+import '@testing-library/jest-dom';
+import { act, cleanup, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import ariaLabelButton from 'src/__tests__/utils-for-testing/aria-labels/aria-label-button';
+import arialabelCheckbox from 'src/__tests__/utils-for-testing/aria-labels/aria-label-checkbox';
+import initialStates from 'src/__tests__/utils-for-testing/data/initial-states';
+import reactTestingLibrary from 'src/__tests__/utils-for-testing/rtl-render/rtl-render-functions';
+import LocalStorageUtils from 'src/utils/localStorage.utils';
 describe('keychainify.component tests:\n', () => {
-  let _asFragment: () => DocumentFragment;
-  const { methods, constants, extraMocks } = keychainify;
-  methods.afterEach;
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.resetModules();
+    cleanup();
+  });
   beforeEach(async () => {
-    _asFragment = await keychainify.beforeEach();
+    await reactTestingLibrary.renderWithConfiguration(
+      <App />,
+      initialStates.iniStateAs.defaultExistent,
+      {
+        app: {
+          ledgerRelated: {
+            LedgerUtils: {
+              isLedgerSupported: true,
+            },
+          },
+        },
+      },
+    );
+    await act(async () => {
+      await userEvent.click(screen.getByLabelText(ariaLabelButton.menu));
+      await userEvent.click(
+        screen.getByLabelText(ariaLabelButton.menuPreFix + Icons.SETTINGS),
+      );
+      await userEvent.click(
+        screen.getByLabelText(ariaLabelButton.menuPreFix + Icons.LINK),
+      );
+    });
   });
   it('Must load component and match show intro message', () => {
-    assertion.getByLabelText(alComponent.advanceSettings.link);
-    assertion.getOneByText(constants.message.intro);
+    expect(
+      screen.getByLabelText(`${Screen.SETTINGS_KEYCHAINIFY}-page`),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(chrome.i18n.getMessage('popup_html_keychainify_text'), {
+        exact: true,
+      }),
+    ).toBeInTheDocument();
   });
-  it('Must set keychainify', async () => {
-    await clickAwait([alCheckbox.keychainify.checkbox]);
-    await waitFor(() => {
-      expect(extraMocks.spy().mock.lastCall).toEqual([
-        'keychainify_enabled',
-        false,
-      ]);
+
+  it('Must call saveValueInLocalStorage', async () => {
+    const sSaveValueInLocalStorage = jest.spyOn(
+      LocalStorageUtils,
+      'saveValueInLocalStorage',
+    );
+    await act(async () => {
+      await userEvent.click(
+        screen.getByLabelText(arialabelCheckbox.keychainify.checkbox),
+      );
     });
+    expect(sSaveValueInLocalStorage.mock.lastCall[0]).toBe(
+      LocalStorageKeyEnum.KEYCHAINIFY_ENABLED,
+    );
+    sSaveValueInLocalStorage.mockRestore();
   });
 });
