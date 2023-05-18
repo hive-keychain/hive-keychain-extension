3cf3e6dae66a269e3fc51515b3a990041ac3554a
theghost1980 <saturnob612@gmail.com>
2023-05-18 06:37:22 -0400

    import-export-preferences.component.test: removed /mocks, refactored

--- src/__tests__/popup/pages/app-container/settings/advanced-settings/import-export-preferences/import-export-preferences.component.test.tsx
+++ src/__tests__/popup/pages/app-container/settings/advanced-settings/import-export-preferences/import-export-preferences.component.test.tsx
@@ -1,31 +1,78 @@
+import App from '@popup/App';
 import { Icons } from '@popup/icons.enum';
-import importExportPreferences from 'src/__tests__/popup/pages/app-container/settings/advanced-settings/import-export-preferences/mocks/import-export-preferences';
-import alButton from 'src/__tests__/utils-for-testing/aria-labels/al-button';
-import alComponent from 'src/__tests__/utils-for-testing/aria-labels/al-component';
-import assertion from 'src/__tests__/utils-for-testing/preset/assertion';
-import config from 'src/__tests__/utils-for-testing/setups/config';
-import { clickAwait } from 'src/__tests__/utils-for-testing/setups/events';
-config.byDefault();
+import { Screen } from '@reference-data/screen.enum';
+import '@testing-library/jest-dom';
+import { act, cleanup, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import ariaLabelButton from 'src/__tests__/utils-for-testing/aria-labels/aria-label-button';
+import initialStates from 'src/__tests__/utils-for-testing/data/initial-states';
+import reactTestingLibrary from 'src/__tests__/utils-for-testing/rtl-render/rtl-render-functions';
+import LocalStorageUtils from 'src/utils/localStorage.utils';
+import SettingsUtils from 'src/utils/settings.utils';
 describe('import-export-preferences.component tests:\n', () => {
-  let _asFragment: () => DocumentFragment;
-  const { methods, constants, extraMocks } = importExportPreferences;
-  methods.afterEach;
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.resetModules();
+    cleanup();
+  });
   beforeEach(async () => {
-    _asFragment = await importExportPreferences.beforeEach();
+    await reactTestingLibrary.renderWithConfiguration(
+      <App />,
+      initialStates.iniStateAs.defaultExistent,
+    );
+    await act(async () => {
+      await userEvent.click(screen.getByLabelText(ariaLabelButton.menu));
+      await userEvent.click(
+        screen.getByLabelText(ariaLabelButton.menuPreFix + Icons.SETTINGS),
+      );
+      await userEvent.click(
+        screen.getByLabelText(ariaLabelButton.menuPreFix + Icons.IMPORT_EXPORT),
+      );
+    });
   });
   it('Must load import-export page and show info', () => {
-    assertion.getByLabelText(alComponent.advanceSettings.importExport);
-    assertion.getOneByText(constants.message.import);
+    expect(
+      screen.getByLabelText(`${Screen.SETTINGS_IMPORT_EXPORT}-page`),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        chrome.i18n.getMessage('popup_html_import_permissions'),
+        { exact: true },
+      ),
+    ).toBeInTheDocument();
   });
+
   it('Must open import window', async () => {
-    await clickAwait([alButton.menuPreFix + Icons.IMPORT]);
-    expect(extraMocks.spy.importSettings).toBeCalledTimes(1);
+    const sImportSettings = jest.spyOn(SettingsUtils, 'importSettings');
+    await act(async () => {
+      await userEvent.click(
+        screen.getByLabelText(ariaLabelButton.menuPreFix + Icons.IMPORT),
+      );
+    });
+    expect(sImportSettings).toHaveBeenCalledTimes(1);
+    sImportSettings.mockRestore();
   });
+
   it('Must try to export settings file', async () => {
-    extraMocks.getMultipleValueFromLocalStorage();
-    extraMocks.aClick;
-    await clickAwait([alButton.menuPreFix + Icons.EXPORT]);
-    expect(extraMocks.spy.exportSettings).toBeCalledTimes(1);
-    expect(extraMocks.aClick).toBeCalledTimes(1);
+    window.URL.createObjectURL = jest
+      .fn()
+      .mockImplementation((...args: any) => args);
+    const sExportSettings = jest.spyOn(SettingsUtils, 'exportSettings');
+    const sClick = jest
+      .spyOn(HTMLAnchorElement.prototype, 'click')
+      .mockImplementation(() => undefined);
+    LocalStorageUtils.getMultipleValueFromLocalStorage = jest
+      .fn()
+      .mockResolvedValueOnce(null);
+    await act(async () => {
+      await userEvent.click(
+        screen.getByLabelText(ariaLabelButton.menuPreFix + Icons.EXPORT),
+      );
+    });
+    expect(sExportSettings).toHaveBeenCalledTimes(1);
+    expect(sClick).toHaveBeenCalledTimes(1);
+    sExportSettings.mockRestore();
+    sClick.mockRestore();
   });
 });
