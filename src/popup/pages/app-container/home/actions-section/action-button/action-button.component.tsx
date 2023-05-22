f107c5c10dfbf77c4b99739b1bf2d668cc407884
theghost1980 <saturnob612@gmail.com>
2023-05-22 10:19:12 -0400

    home tests related: removed /mocks, action-button updated as same pattern for menu tests

--- src/popup/pages/app-container/home/actions-section/action-button/action-button.component.tsx
+++ src/popup/pages/app-container/home/actions-section/action-button/action-button.component.tsx
@@ -16,9 +16,7 @@ const ActionButton = ({
 }: PropsType) => {
   return (
     <div
-      aria-label={`action-button-${chrome.i18n
-        .getMessage(label)
-        .toLowerCase()}`}
+      aria-label={`action-button-${icon}`}
       className="action-button"
       onClick={() => navigateToWithParams(nextScreen, nextScreenParams)}>
       <div className="icon-container">
