b38484bf8efd3c9e912202622649193124567960
theghost1980 <saturnob612@gmail.com>
2023-05-22 07:31:59 -0400

    home.component.dropdown-hp.test:  refactored

--- src/__tests__/popup/pages/app-container/home/cases/home/home.component.dropdown-hp.test.tsx
+++ src/__tests__/popup/pages/app-container/home/cases/home/home.component.dropdown-hp.test.tsx
@@ -1,31 +1,78 @@
 import App from '@popup/App';
+import { Icons } from '@popup/icons.enum';
+import { HpDropdownMenuItems } from '@popup/pages/app-container/home/wallet-info-section/wallet-info-dropdown-menus.list';
+import { Screen } from '@reference-data/screen.enum';
 import '@testing-library/jest-dom';
+import { act, cleanup, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
-import home from 'src/__tests__/popup/pages/app-container/home/mocks/home/home';
-import alDropdown from 'src/__tests__/utils-for-testing/aria-labels/al-dropdown';
-import alIcon from 'src/__tests__/utils-for-testing/aria-labels/al-icon';
+import ariaLabelDropdown from 'src/__tests__/utils-for-testing/aria-labels/aria-label-dropdown';
+import ariaLabelIcon from 'src/__tests__/utils-for-testing/aria-labels/aria-label-icon';
 import accounts from 'src/__tests__/utils-for-testing/data/accounts';
-import assertion from 'src/__tests__/utils-for-testing/preset/assertion';
-import afterTests from 'src/__tests__/utils-for-testing/setups/afterTests';
-import config from 'src/__tests__/utils-for-testing/setups/config';
-import { clickAwait } from 'src/__tests__/utils-for-testing/setups/events';
-config.byDefault();
+import initialStates from 'src/__tests__/utils-for-testing/data/initial-states';
+import reactTestingLibrary from 'src/__tests__/utils-for-testing/rtl-render/rtl-render-functions';
+import CurrencyUtils from 'src/utils/currency.utils';
 describe('home.component dropdown hp tests:\n', () => {
-  const { hpDropdownLabelPage } = home;
   beforeEach(async () => {
-    await home.beforeEach(<App />, accounts.twoAccounts);
-    await clickAwait([alDropdown.arrow.hp]);
+    await reactTestingLibrary.renderWithConfiguration(
+      <App />,
+      initialStates.iniStateAs.defaultExistent,
+      {
+        app: {
+          accountsRelated: {
+            AccountUtils: {
+              getAccountsFromLocalStorage: accounts.twoAccounts,
+            },
+          },
+        },
+      },
+    );
+    await act(async () => {
+      await userEvent.click(
+        screen.getByLabelText(
+          ariaLabelDropdown.arrow.preFix +
+            CurrencyUtils.getCurrencyLabels(false).hp.toLowerCase(),
+        ),
+      );
+    });
   });
   afterEach(() => {
-    afterTests.clean();
+    jest.clearAllMocks();
+    jest.resetModules();
+    cleanup();
   });
   it('Must open each page when clicking on menu item', async () => {
-    for (let i = 0; i < hpDropdownLabelPage.length; i++) {
-      const { ariaLabelPreFixed, pageComponent } = hpDropdownLabelPage[i];
-      await clickAwait([ariaLabelPreFixed]);
-      assertion.getByLabelText(pageComponent);
-      await clickAwait([alIcon.closePage]);
-      await clickAwait([alDropdown.arrow.hp]);
+    for (let i = 0; i < HpDropdownMenuItems.length; i++) {
+      await act(async () => {
+        await userEvent.click(
+          screen.getByLabelText(
+            ariaLabelDropdown.walletInfo.preFix + HpDropdownMenuItems[i].icon,
+          ),
+        );
+      });
+      if (
+        HpDropdownMenuItems[i].icon === Icons.ARROW_DOWNWARDS ||
+        HpDropdownMenuItems[i].icon === Icons.ARROW_UPWARDS
+      ) {
+        expect(
+          await screen.findByLabelText(`${Screen.POWER_UP_PAGE}-page`),
+        ).toBeInTheDocument();
+      } else {
+        expect(
+          await screen.findByLabelText(
+            `${HpDropdownMenuItems[i].nextScreen}-page`,
+          ),
+        ).toBeInTheDocument();
+      }
+      await act(async () => {
+        await userEvent.click(screen.getByLabelText(ariaLabelIcon.closePage));
+        await userEvent.click(
+          screen.getByLabelText(
+            ariaLabelDropdown.arrow.preFix +
+              CurrencyUtils.getCurrencyLabels(false).hp.toLowerCase(),
+          ),
+        );
+      });
     }
   });
 });
