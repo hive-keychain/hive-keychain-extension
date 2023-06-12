e0a6aa291d0435b00f45e4218bc3f701275f5bb3
theghost1980 <saturnob612@gmail.com>
2023-06-12 10:13:19 -0400

    resources-section.component.test: removed /mocks, refactored

--- src/__tests__/popup/pages/app-container/home/resources-section/resources-section.component.test.tsx
+++ src/__tests__/popup/pages/app-container/home/resources-section/resources-section.component.test.tsx
@@ -1,61 +1,125 @@
+import { ExtendedAccount } from '@hiveio/dhive';
+import { RC } from '@interfaces/active-account.interface';
 import App from '@popup/App';
-import { screen } from '@testing-library/react';
+import { Icons } from '@popup/icons.enum';
+import '@testing-library/jest-dom';
+import { act, cleanup, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
-import resourcesSection from 'src/__tests__/popup/pages/app-container/home/resources-section/mocks/resources-section';
-import alDiv from 'src/__tests__/utils-for-testing/aria-labels/al-div';
-import alToolTip from 'src/__tests__/utils-for-testing/aria-labels/al-toolTip';
-import { EventType } from 'src/__tests__/utils-for-testing/enums/enums';
-import assertion from 'src/__tests__/utils-for-testing/preset/assertion';
-import config from 'src/__tests__/utils-for-testing/setups/config';
-import { clickTypeAwait } from 'src/__tests__/utils-for-testing/setups/events';
-config.byDefault();
+import ariaLabelDiv from 'src/__tests__/utils-for-testing/aria-labels/aria-label-div';
+import ariaLabelToolTip from 'src/__tests__/utils-for-testing/aria-labels/aria-label-tool-tip';
+import accounts from 'src/__tests__/utils-for-testing/data/accounts';
+import initialStates from 'src/__tests__/utils-for-testing/data/initial-states';
+import fake_RC from 'src/__tests__/utils-for-testing/data/rc';
+import reactTestingLibrary from 'src/__tests__/utils-for-testing/rtl-render/rtl-render-functions';
+import HiveUtils from 'src/utils/hive.utils';
+
 describe('resources-section.component tests:\n', () => {
-  const { methods, constants } = resourcesSection;
-  methods.afterEach;
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.resetModules();
+    cleanup();
+  });
+  const extendedAccountVestingsAsString = {
+    ...accounts.extended,
+    vesting_shares: '10000 VESTS',
+    received_vesting_shares: '20000 VESTS',
+    delegated_vesting_shares: '100 VESTS',
+  };
+  const mana = HiveUtils.getVP(extendedAccountVestingsAsString);
+
   describe('existing data\n:', () => {
     beforeEach(async () => {
-      await resourcesSection.beforeEach(<App />);
+      await reactTestingLibrary.renderWithConfiguration(
+        <App />,
+        initialStates.iniStateAs.defaultExistent,
+      );
     });
-    it('Must show mana and resource credits', () => {
-      assertion.toHaveTextContent([
-        { arialabel: alDiv.resources.vm, text: constants.wMana },
-        { arialabel: alDiv.resources.rc, text: constants.wRc },
-      ]);
+    it('Must show mana and resource credits, labels & values', async () => {
+      expect(
+        await screen.findByLabelText(ariaLabelDiv.resources.vm),
+      ).toHaveTextContent(
+        `${Icons.ARROW_UP}${chrome.i18n.getMessage(
+          'popup_html_vm',
+        )}1.00 % (1.00 $)`,
+      );
+      expect(
+        await screen.findByLabelText(ariaLabelDiv.resources.rc),
+      ).toHaveTextContent(
+        `${Icons.RC}${chrome.i18n.getMessage('popup_html_rc')}100.00 %`,
+      );
     });
+
     it('Must show tool tip when hover on mana', async () => {
-      await clickTypeAwait([
-        { ariaLabel: alDiv.resources.vm, event: EventType.HOVER },
-      ]);
-      expect(screen.getByLabelText(alToolTip.content)).toHaveTextContent(
-        constants.toolTip.fullInDays,
-      );
+      await act(async () => {
+        await userEvent.hover(screen.getByLabelText(ariaLabelDiv.resources.vm));
+      });
+      expect(
+        await screen.findByLabelText(ariaLabelToolTip.content),
+      ).toHaveTextContent(HiveUtils.getTimeBeforeFull(mana!));
     });
+
     it('Must show tool tip when hover on credits', async () => {
-      await clickTypeAwait([
-        { ariaLabel: alDiv.resources.rc, event: EventType.HOVER },
-      ]);
-      expect(screen.getByLabelText(alToolTip.content)).toHaveTextContent(
-        constants.toolTip.fullIn,
-      );
+      await act(async () => {
+        await userEvent.hover(screen.getByLabelText(ariaLabelDiv.resources.rc));
+      });
+      expect(
+        await screen.findByLabelText(ariaLabelToolTip.content),
+      ).toHaveTextContent(HiveUtils.getTimeBeforeFull(fake_RC.rc.percentage));
     });
   });
+
   describe('zero as data\n:', () => {
+    const extendedAccountZero = {
+      ...accounts.extended,
+      voting_manabar: {
+        current_mana: 0,
+      },
+    } as ExtendedAccount;
+    const rcZero = {
+      current_mana: 0,
+      percentage: 0,
+      delegated_rc: 0,
+      received_delegated_rc: 0,
+      max_rc: 0,
+      max_mana: 0,
+    } as RC;
     beforeEach(async () => {
-      await resourcesSection.beforeEach(<App />, true);
-    });
-    it('Must show -- as mana and credits 0', () => {
-      assertion.toHaveTextContent([
-        { arialabel: alDiv.resources.vm, text: constants.noMana },
-        { arialabel: alDiv.resources.rc, text: constants.zeroRc },
-      ]);
-    });
-    it('Must show no hp tool tip when hover on mana', async () => {
-      await clickTypeAwait([
-        { ariaLabel: alDiv.resources.vm, event: EventType.HOVER },
-      ]);
-      expect(screen.getByLabelText(alToolTip.content)).toHaveTextContent(
-        constants.toolTip.noHp,
+      await reactTestingLibrary.renderWithConfiguration(
+        <App />,
+        initialStates.iniStateAs.defaultExistent,
+        {
+          app: {
+            accountsRelated: {
+              AccountUtils: {
+                getExtendedAccount: extendedAccountZero,
+                getRCMana: rcZero,
+              },
+            },
+          },
+        },
       );
+    });
+    it('Must show -- as mana and credits 0', async () => {
+      expect(
+        await screen.findByLabelText(ariaLabelDiv.resources.vm),
+      ).toHaveTextContent(
+        `${Icons.ARROW_UP}${chrome.i18n.getMessage('popup_html_vm')}--`,
+      );
+      expect(
+        await screen.findByLabelText(ariaLabelDiv.resources.rc),
+      ).toHaveTextContent(
+        `${Icons.RC}${chrome.i18n.getMessage('popup_html_rc')}0.00 %`,
+      );
+    });
+
+    it('Must show no hp tool tip when hover on mana', async () => {
+      await act(async () => {
+        await userEvent.hover(screen.getByLabelText(ariaLabelDiv.resources.vm));
+      });
+      expect(
+        await screen.findByLabelText(ariaLabelToolTip.content),
+      ).toHaveTextContent(chrome.i18n.getMessage('html_popup_voting_no_hp'));
     });
   });
 });
