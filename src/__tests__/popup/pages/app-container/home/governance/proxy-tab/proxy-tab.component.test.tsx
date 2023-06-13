6faf46856ff3d392c0c1a459a807926089cad153
theghost1980 <saturnob612@gmail.com>
2023-06-13 09:41:57 -0400

    proxy-tab.component.test: removed /mocks, refactored

--- src/__tests__/popup/pages/app-container/home/governance/proxy-tab/proxy-tab.component.test.tsx
+++ src/__tests__/popup/pages/app-container/home/governance/proxy-tab/proxy-tab.component.test.tsx
@@ -1,80 +1,224 @@
+import { TransactionResult } from '@interfaces/hive-tx.interface';
+import { LocalAccount } from '@interfaces/local-account.interface';
 import App from '@popup/App';
+import { Icons } from '@popup/icons.enum';
 import '@testing-library/jest-dom';
-import { screen } from '@testing-library/react';
+import { act, cleanup, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { KeychainKeyTypesLC } from 'hive-keychain-commons';
 import React from 'react';
-import proxy from 'src/__tests__/popup/pages/app-container/home/governance/proxy-tab/mocks/proxy';
-import initialProxy from 'src/__tests__/popup/pages/app-container/home/governance/proxy-tab/othercases/initial-proxy';
-import noActiveKey from 'src/__tests__/popup/pages/app-container/home/governance/proxy-tab/othercases/no-active-key';
-import alIcon from 'src/__tests__/utils-for-testing/aria-labels/al-icon';
-import alInput from 'src/__tests__/utils-for-testing/aria-labels/al-input';
-import {
-  EventType,
-  QueryDOM,
-} from 'src/__tests__/utils-for-testing/enums/enums';
-import assertion from 'src/__tests__/utils-for-testing/preset/assertion';
-import config from 'src/__tests__/utils-for-testing/setups/config';
-import { clickTypeAwait } from 'src/__tests__/utils-for-testing/setups/events';
-config.byDefault();
+import ariaLabelButton from 'src/__tests__/utils-for-testing/aria-labels/aria-label-button';
+import ariaLabelInput from 'src/__tests__/utils-for-testing/aria-labels/aria-label-input';
+import accounts from 'src/__tests__/utils-for-testing/data/accounts';
+import initialStates from 'src/__tests__/utils-for-testing/data/initial-states';
+import objects from 'src/__tests__/utils-for-testing/helpers/objects';
+import reactTestingLibrary from 'src/__tests__/utils-for-testing/rtl-render/rtl-render-functions';
+import ProxyUtils from 'src/utils/proxy.utils';
+
 describe('proxy-tab.component tests:\n', () => {
-  const { methods, extraMocks, constants } = proxy;
-  methods.afterEach;
-  describe('with active key cases:\n', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.resetModules();
+    cleanup();
+  });
+  describe('With active key cases:\n', () => {
     describe('empty proxy', () => {
       beforeEach(async () => {
-        await proxy.beforeEach(<App />, false, false);
-      });
-      it('Must show intro message for empty proxy', () => {
-        assertion.getOneByText(constants.introMessage.noProxy);
-      });
-      it('Must change the input username when typying', async () => {
-        await clickTypeAwait([
+        await reactTestingLibrary.renderWithConfiguration(
+          <App />,
+          initialStates.iniStateAs.defaultExistent,
           {
-            ariaLabel: alInput.username,
-            event: EventType.TYPE,
-            text: 'keychain',
+            app: {
+              accountsRelated: {
+                AccountUtils: {
+                  getExtendedAccount: { ...accounts.extended, proxy: '' },
+                },
+              },
+            },
           },
-        ]);
-        assertion.getByDisplay('keychain');
+        );
+        await act(async () => {
+          await userEvent.click(screen.getByLabelText(ariaLabelButton.menu));
+          await userEvent.click(
+            screen.getByLabelText(ariaLabelButton.menuPreFix + Icons.HIVE),
+          );
+          await userEvent.click(screen.getAllByRole('tab')[1]);
+        });
       });
-      it('Must clear the input when clicking clear icon', async () => {
-        await clickTypeAwait([
-          {
-            ariaLabel: alInput.username,
-            event: EventType.TYPE,
-            text: 'keychain',
-          },
-        ]);
-        assertion.getByDisplay('keychain');
-        await clickTypeAwait([
-          {
-            ariaLabel: alIcon.input.clear,
-            event: EventType.CLICK,
-          },
-        ]);
-        expect(screen.getByLabelText(alInput.username)).toHaveTextContent('');
+
+      it('Must show intro message for empty proxy', async () => {
+        expect(
+          await screen.findByText(
+            chrome.i18n
+              .getMessage('html_popup_witness_proxy_definition')
+              .trim(),
+            { exact: true },
+          ),
+        ).toBeInTheDocument();
       });
+
       it('Must set proxy and show message', async () => {
-        extraMocks({ setAsProxy: true });
-        await methods.typeNClick('keychain');
-        await assertion.awaitFor(constants.successMessage, QueryDOM.BYTEXT);
+        ProxyUtils.setAsProxy = jest.fn().mockResolvedValue({
+          tx_id: 'tx_id',
+          id: 'id',
+          confirmed: true,
+        } as TransactionResult);
+        await act(async () => {
+          await userEvent.type(
+            screen.getByLabelText(ariaLabelInput.username),
+            'keychain',
+          );
+          await userEvent.click(
+            screen.getByLabelText(
+              ariaLabelButton.operation.proxy.tab.setAsProxy,
+            ),
+          );
+        });
+        expect(
+          await screen.findByText(
+            chrome.i18n.getMessage('popup_success_proxy', ['keychain']),
+          ),
+        ).toBeInTheDocument();
       });
+
       it('Must show error when set proxy fails', async () => {
-        extraMocks({ setAsProxy: false });
-        await methods.typeNClick('keychain');
-        await assertion.awaitFor(constants.errorMessage.set, QueryDOM.BYTEXT);
+        ProxyUtils.setAsProxy = jest.fn().mockResolvedValue(null);
+        await act(async () => {
+          await userEvent.type(
+            screen.getByLabelText(ariaLabelInput.username),
+            'keychain',
+          );
+          await userEvent.click(
+            screen.getByLabelText(
+              ariaLabelButton.operation.proxy.tab.setAsProxy,
+            ),
+          );
+        });
+        expect(
+          await screen.findByText(
+            chrome.i18n.getMessage('html_popup_set_as_proxy_error'),
+          ),
+        ).toBeInTheDocument();
+      });
+
+      it('Must catch error and show', async () => {
+        ProxyUtils.setAsProxy = jest
+          .fn()
+          .mockRejectedValue(
+            new Error('Error when setting proxy in proxy-tab'),
+          );
+        await act(async () => {
+          await userEvent.type(
+            screen.getByLabelText(ariaLabelInput.username),
+            'keychain',
+          );
+          await userEvent.click(
+            screen.getByLabelText(
+              ariaLabelButton.operation.proxy.tab.setAsProxy,
+            ),
+          );
+        });
+        expect(
+          await screen.findByText('Error when setting proxy in proxy-tab'),
+        ).toBeInTheDocument();
       });
     });
-    describe('initial proxy', () => {
+
+    describe('Initial proxy', () => {
       beforeEach(async () => {
-        await proxy.beforeEach(<App />, false, true);
+        await reactTestingLibrary.renderWithConfiguration(
+          <App />,
+          initialStates.iniStateAs.defaultExistent,
+          {
+            app: {
+              accountsRelated: {
+                AccountUtils: {
+                  getExtendedAccount: {
+                    ...accounts.extended,
+                    proxy: 'keychain',
+                  },
+                },
+                ProxyUtils: {
+                  findUserProxy: 'keychain',
+                },
+              },
+            },
+          },
+        );
+        await act(async () => {
+          await userEvent.click(screen.getByLabelText(ariaLabelButton.menu));
+          await userEvent.click(
+            screen.getByLabelText(ariaLabelButton.menuPreFix + Icons.HIVE),
+          );
+          await userEvent.click(screen.getAllByRole('tab')[1]);
+        });
       });
-      initialProxy.run();
+
+      it('Must show intro message and current proxy account', async () => {
+        expect(
+          await screen.findByText(
+            chrome.i18n.getMessage('html_popup_witness_has_proxy').trim(),
+            { exact: true },
+          ),
+        ).toBeInTheDocument();
+        expect(
+          await screen.findByText(
+            chrome.i18n.getMessage('html_popup_currently_using_proxy', [
+              'keychain',
+            ]),
+          ),
+        ).toBeInTheDocument();
+      });
     });
   });
-  describe('no active key cases:\n', () => {
+
+  describe('No active key cases:\n', () => {
     beforeEach(async () => {
-      await proxy.beforeEach(<App />, true, false);
+      const cloneLocalAccounts = objects.clone(
+        accounts.twoAccounts,
+      ) as LocalAccount[];
+      delete cloneLocalAccounts[0].keys.active;
+      delete cloneLocalAccounts[0].keys.activePubkey;
+      await reactTestingLibrary.renderWithConfiguration(
+        <App />,
+        initialStates.iniStateAs.defaultExistent,
+        {
+          app: {
+            accountsRelated: {
+              AccountUtils: {
+                getExtendedAccount: { ...accounts.extended, proxy: '' },
+                getAccountsFromLocalStorage: cloneLocalAccounts,
+              },
+            },
+          },
+        },
+      );
+      await act(async () => {
+        await userEvent.click(screen.getByLabelText(ariaLabelButton.menu));
+        await userEvent.click(
+          screen.getByLabelText(ariaLabelButton.menuPreFix + Icons.HIVE),
+        );
+        await userEvent.click(screen.getAllByRole('tab')[1]);
+      });
     });
-    noActiveKey.run();
+
+    it('Must show error trying to set proxy', async () => {
+      await act(async () => {
+        await userEvent.type(
+          screen.getByLabelText(ariaLabelInput.username),
+          'keychain',
+        );
+        await userEvent.click(
+          screen.getByLabelText(ariaLabelButton.operation.proxy.tab.setAsProxy),
+        );
+      });
+      expect(
+        await screen.findByText(
+          chrome.i18n.getMessage('popup_missing_key', [
+            KeychainKeyTypesLC.active,
+          ]),
+        ),
+      ).toBeInTheDocument();
+    });
   });
 });
