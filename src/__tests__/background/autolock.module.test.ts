b3d9d1d6b03c7efe2dd6c8ef2bf3e732647ce7dc
theghost1980 <saturnob612@gmail.com>
2023-06-16 10:46:26 -0400

    autolock.module.test: removed /mocks, refactored

--- src/__tests__/background/autolock.module.test.ts
+++ src/__tests__/background/autolock.module.test.ts
@@ -1,32 +1,65 @@
 import AutolockModule from '@background/autolock.module';
-import { Autolock } from '@interfaces/autolock.interface';
-import autolockModuleMocks from 'src/__tests__/background/mocks/autolock.module.mocks';
+import { AutoLockType, Autolock } from '@interfaces/autolock.interface';
+import LocalStorageUtils from 'src/utils/localStorage.utils';
+import Logger from 'src/utils/logger.utils';
+
 describe('autolock.module tests:\n', () => {
-  const { mocks, methods, spies, constants } = autolockModuleMocks;
-  const { loggerMsg, autoLock } = constants;
-  methods.afterEach;
+  const constants = {
+    loggerMsg: (autoLock: Autolock) =>
+      `hive-keychain: setting up ${autoLock.type} listener ${
+        autoLock.type === AutoLockType.IDLE_LOCK ? `(${autoLock.mn}mn)` : ''
+      }`,
+    autoLock: {
+      deviceLock: { type: AutoLockType.DEVICE_LOCK, mn: 10 } as Autolock,
+      idleLock: { type: AutoLockType.IDLE_LOCK, mn: 60 } as Autolock,
+    },
+  };
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.resetModules();
+    jest.restoreAllMocks();
+    jest.resetAllMocks();
+  });
   describe('set cases:\n', () => {
     it('Must return undefined', async () => {
+      const sLoggerInfo = jest.spyOn(Logger, 'info');
       expect(await AutolockModule.set({} as Autolock)).toBeUndefined();
-      expect(spies.logger).not.toBeCalled();
+      expect(sLoggerInfo).not.toBeCalled();
     });
+
     it('Must set autoLock as Device', async () => {
-      await AutolockModule.set(autoLock.deviceLock);
-      expect(spies.logger).toBeCalledWith(loggerMsg(autoLock.deviceLock));
+      const sLoggerInfo = jest.spyOn(Logger, 'info');
+      await AutolockModule.set(constants.autoLock.deviceLock);
+      expect(sLoggerInfo).toBeCalledWith(
+        `hive-keychain: setting up ${constants.autoLock.deviceLock.type} listener `,
+      );
     });
+
     it('Must set autlock as Idle and set interval', async () => {
-      await AutolockModule.set(autoLock.idleLock);
-      expect(spies.logger).toBeCalledWith(loggerMsg(autoLock.idleLock));
-      expect(spies.setDetectionInterval).toBeCalledWith(
-        autoLock.idleLock.mn * 60,
+      const sLoggerInfo = jest.spyOn(Logger, 'info');
+      const setDetectionInterval = jest
+        .spyOn(chrome.idle, 'setDetectionInterval')
+        .mockReturnValue(undefined);
+
+      await AutolockModule.set(constants.autoLock.idleLock);
+      expect(sLoggerInfo).toBeCalledWith(
+        `hive-keychain: setting up ${constants.autoLock.idleLock.type} listener (${constants.autoLock.idleLock.mn}mn)`,
+      );
+      expect(setDetectionInterval).toBeCalledWith(
+        constants.autoLock.idleLock.mn * 60,
       );
     });
   });
+
   describe('start cases:\n', () => {
     it('Must call Logger and return undefined', async () => {
-      mocks.getValueFromLocalStorage(undefined);
+      LocalStorageUtils.getValueFromLocalStorage = jest
+        .fn()
+        .mockResolvedValue(undefined);
+      const sLoggerInfo = jest.spyOn(Logger, 'info');
       expect(await AutolockModule.start()).toBeUndefined();
-      expect(spies.logger).toBeCalledWith('Starting autolock');
+      expect(sLoggerInfo).toBeCalledWith('Starting autolock');
     });
   });
 });
