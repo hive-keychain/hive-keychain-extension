7766d803f275f81a6e0f89456d3f21a01cc59b7a
theghost1980 <saturnob612@gmail.com>
2023-06-16 09:05:33 -0400

    ledger.module.test: removed /mocks, refactored

--- src/__tests__/background/ledger.module.test.ts
+++ src/__tests__/background/ledger.module.test.ts
@@ -1,13 +1,34 @@
 import LedgerModule from '@background/ledger.module';
 import { DialogCommand } from '@reference-data/dialog-message-key.enum';
-import ledgerModuleMocks from 'src/__tests__/background/mocks/ledger.module-mocks';
+import userData from 'src/__tests__/utils-for-testing/data/user-data';
+import { SignFromLedgerRequestMessage } from 'src/dialog/pages/sign-transaction';
 
 describe('ledger module tests:\n', () => {
-  const { data, spies } = ledgerModuleMocks;
+  const data: SignFromLedgerRequestMessage = {
+    transaction: {
+      ref_block_num: 1,
+      ref_block_prefix: 1,
+      expiration: '11221112',
+      operations: [],
+      extensions: [],
+    },
+    key: userData.one.nonEncryptKeys.posting,
+  };
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.resetModules();
+    jest.restoreAllMocks();
+    jest.resetAllMocks();
+  });
+
   describe('signTransactionFromLedger cases:\n', () => {
     it('Must call sendMessage', () => {
+      const sSendMessage = jest
+        .spyOn(chrome.runtime, 'sendMessage')
+        .mockReturnValue(undefined);
+
       LedgerModule.signTransactionFromLedger(data);
-      expect(spies.sendMessage).toBeCalledWith({
+      expect(sSendMessage).toBeCalledWith({
         command: DialogCommand.SIGN_WITH_LEDGER,
         ...data,
       });
