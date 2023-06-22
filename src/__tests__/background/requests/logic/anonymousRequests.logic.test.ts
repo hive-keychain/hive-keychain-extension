9b398e8b61f523e4fbcc758bb95c9c00de80a5d2
theghost1980 <saturnob612@gmail.com>
2023-06-22 06:41:04 -0400

    /logic/anonymousRequests.logic.test: removed /mocks, refactored

--- src/__tests__/background/requests/logic/anonymousRequests.logic.test.ts
+++ src/__tests__/background/requests/logic/anonymousRequests.logic.test.ts
@@ -1,33 +1,59 @@
 import { anonymousRequests } from '@background/requests/logic';
-import anonymousRequestsLogic from 'src/__tests__/background/requests/logic/mocks/anonymousRequests.logic';
+import { RequestsHandler } from '@background/requests/request-handler';
+import { Rpc } from '@interfaces/rpc.interface';
 import accounts from 'src/__tests__/utils-for-testing/data/accounts';
+import keychainRequest from 'src/__tests__/utils-for-testing/data/keychain-request';
+import * as DialogLifeCycle from 'src/background/requests/dialog-lifecycle';
+
 describe('anonymousRequests.logic tests:\n', () => {
-  const { constants, spies, callback, methods } = anonymousRequestsLogic;
-  methods.afterEach;
-  it('If empty account must call sendErrors', () => {
-    anonymousRequests(
-      constants.requestHandler,
-      constants.tab,
-      constants.request,
-      constants.domain,
-      constants.account_candidates.empty,
-      constants.current_rpc,
-    );
-    expect(
-      methods.clean(spies.createPopup.mock.calls[0][0].toString()),
-    ).toEqual(methods.clean(callback.sendErrors.toString()));
+  // const { constants, spies, callback, methods } = anonymousRequestsLogic;
+  // methods.afterEach;
+  const account_candidates = {
+    empty: [],
+    two: ['one', 'two'],
+  };
+  const current_rpc = {} as Rpc;
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.resetModules();
+    jest.restoreAllMocks();
+    jest.resetAllMocks();
   });
-  it('Must call createPopup with sendMessage callback', () => {
+
+  it('If empty account must call sendErrors', () => {
+    const sCreatePopup = jest
+      .spyOn(DialogLifeCycle, 'createPopup')
+      .mockImplementation(() => undefined);
+    const requestHandler = new RequestsHandler();
     anonymousRequests(
-      constants.requestHandler,
-      constants.tab,
-      constants.request,
-      constants.domain,
-      accounts.twoAccounts,
-      constants.current_rpc,
+      requestHandler,
+      0,
+      keychainRequest.noValues.decode,
+      'domain',
+      account_candidates.empty,
+      current_rpc,
     );
-    expect(
-      methods.clean(spies.createPopup.mock.calls[0][0].toString()),
-    ).toEqual(methods.clean(callback.sendMessage.toString()));
+    expect(sCreatePopup.mock.calls[0][0].toString()).toContain(
+      'bgd_auth_no_active',
+    );
+  });
+
+  it('Must call createPopup with sendMessage callback', () => {
+    const sCreatePopup = jest
+      .spyOn(DialogLifeCycle, 'createPopup')
+      .mockImplementation(() => undefined);
+    const requestHandler = new RequestsHandler();
+    anonymousRequests(
+      requestHandler,
+      0,
+      keychainRequest.noValues.decode,
+      'domain',
+      accounts.twoAccounts,
+      current_rpc,
+    );
+    expect(sCreatePopup.mock.calls[0][0].toString()).toContain(
+      'chrome.runtime.sendMessage',
+    );
   });
 });
