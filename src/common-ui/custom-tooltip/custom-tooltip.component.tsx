7bf7bb50c73837a54f1433339ee1637d056a9e80
Cedric Gui <cedric.guillas78@gmail.com>
2023-08-28 11:01:53 +0800

    feat(tooltip): Multiple fixes

--- src/common-ui/custom-tooltip/custom-tooltip.component.tsx
+++ src/common-ui/custom-tooltip/custom-tooltip.component.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './custom-tooltip.component.scss';
 
 export type CustomTooltipPosition = 'top' | 'bottom' | 'left' | 'right';
@@ -13,6 +13,10 @@ interface TooltipProps {
   additionalClassName?: string;
 }
 
+const TOOLTIP_WIDTH = 150;
+const TOOLTIP_PADDING = 12;
+const TOOLTIP_ARROW_SIZE = 6;
+
 export const CustomTooltip = ({
   position,
   delayShow,
@@ -23,10 +27,68 @@ export const CustomTooltip = ({
   dataTestId,
   additionalClassName,
 }: TooltipProps) => {
+  const anchor = useRef<HTMLDivElement>(null);
+  const tooltip = useRef<HTMLDivElement>(null);
+
   const [isOpen, setOpen] = useState(false);
   let timeout: NodeJS.Timeout;
+  let [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    if (isOpen && (!position || position === 'top')) {
+      const divInfo = anchor.current?.getBoundingClientRect();
+      const tooltipInfo = tooltip.current?.getBoundingClientRect();
+      if (divInfo && tooltipInfo) {
+        setCoordinates({
+          x: divInfo.left + divInfo.width / 2 - TOOLTIP_ARROW_SIZE,
+          y: divInfo.top - tooltipInfo.height - TOOLTIP_ARROW_SIZE - 2,
+        });
+      }
+    }
+  }, [isOpen]);
 
   const show = () => {
+    const divInfo = anchor.current?.getBoundingClientRect();
+
+    if (!divInfo) return;
+
+    switch (position) {
+      case 'left': {
+        setCoordinates({
+          x:
+            divInfo.left -
+            TOOLTIP_WIDTH -
+            2 * TOOLTIP_PADDING -
+            TOOLTIP_ARROW_SIZE -
+            2,
+          y: divInfo.top + divInfo.height / 2,
+        });
+        break;
+      }
+      case 'right': {
+        setCoordinates({
+          x: divInfo.left + divInfo.width + TOOLTIP_ARROW_SIZE + 2,
+          y: divInfo.top + divInfo.height / 2,
+        });
+        break;
+      }
+      case 'bottom': {
+        setCoordinates({
+          x: divInfo.left + divInfo.width / 2 - TOOLTIP_ARROW_SIZE,
+          y: divInfo.top + divInfo.height + TOOLTIP_ARROW_SIZE,
+        });
+        break;
+      }
+      case 'top':
+      default: {
+        setCoordinates({
+          x: divInfo.left + divInfo.width / 2,
+          y: divInfo.top,
+        });
+        break;
+      }
+    }
+
     timeout = setTimeout(
       () => {
         setOpen(true);
@@ -45,17 +107,63 @@ export const CustomTooltip = ({
       className={`tooltip-container ${additionalClassName}`}
       onMouseEnter={show}
       onMouseLeave={hide}>
-      <div className="tooltip-anchor">{children}</div>
+      <div className="tooltip-anchor" ref={anchor}>
+        {children}
+      </div>
+
       {isOpen && message && (
         <div
+          ref={tooltip}
           data-testid="tooltip-content"
           className={`tooltip ${position ? position : 'top'}`}
+          style={{
+            position: 'fixed',
+            top: coordinates.y,
+            left: coordinates.x,
+          }}
           dangerouslySetInnerHTML={{
             __html: skipTranslation
               ? message
               : chrome.i18n.getMessage(message, messageParams),
           }}></div>
       )}
+
+      {/* <div
+        style={{
+          width: 4,
+          height: 4,
+          background: 'blue',
+          position: 'fixed',
+          top: left.y,
+          left: left.x,
+        }}></div>
+      <div
+        style={{
+          width: 4,
+          height: 4,
+          background: 'blue',
+          position: 'fixed',
+          top: right.y,
+          left: right.x,
+        }}></div>
+      <div
+        style={{
+          width: 4,
+          height: 4,
+          background: 'blue',
+          position: 'fixed',
+          top: top.y,
+          left: top.x,
+        }}></div>
+      <div
+        style={{
+          width: 4,
+          height: 4,
+          background: 'blue',
+          position: 'fixed',
+          top: bottom.y,
+          left: bottom.x,
+        }}></div> */}
     </div>
   );
 };
