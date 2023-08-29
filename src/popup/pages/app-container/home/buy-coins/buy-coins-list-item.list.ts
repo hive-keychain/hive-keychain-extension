e639d302753d6dbac77513173fb98b968a9483de
stoodkev <quentin.correa@gmail.com>
2023-08-29 16:35:11 +0800

    change binance account

--- src/popup/pages/app-container/home/buy-coins/buy-coins-list-item.list.ts
+++ src/popup/pages/app-container/home/buy-coins/buy-coins-list-item.list.ts
@@ -14,7 +14,7 @@ export const exchanges = [
     name: 'Binance',
     image: 'binance.svg',
     link: 'https://www.binance.com/en/trade/HIVE_BTC',
-    username: 'deepcrypto8',
+    username: 'bdhivesteem',
     acceptedCoins: ['HIVE'],
   },
   {
