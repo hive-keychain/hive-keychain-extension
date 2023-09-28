e734d389d11d834ca4c6ecfa497377f17040387e
Cedric Gui <cedric.guillas78@gmail.com>
2023-09-28 09:38:49 +0800

    ui(pending conversion): updated ui

--- src/popup/hive/pages/app-container/home/conversion/pending-conversion/pending-conversion.component.tsx
+++ src/popup/hive/pages/app-container/home/conversion/pending-conversion/pending-conversion.component.tsx
@@ -23,33 +23,40 @@ const PendingConversionPage = ({
     <div
       className="pending-conversion-page"
       data-testid={`${Screen.PENDING_CONVERSION_PAGE}-page`}>
-      {navParams.pendingConversions.map((pendingConversion: Conversion) => (
-        <CustomTooltip
-          dataTestId="tooltip-timesteamp-pending-conversion"
-          position="bottom"
-          key={pendingConversion.id}
-          message={chrome.i18n.getMessage(
-            'popup_html_pending_currency_timestamp',
-            [
-              moment.utc(pendingConversion.conversion_date).local().format('L'),
-              moment
-                .utc(pendingConversion.conversion_date)
-                .local()
-                .format('LT'),
-            ],
-          )}
-          skipTranslation>
-          <div className="pending-undelegation">
-            <div className="expiration-date">
-              {moment
-                .utc(pendingConversion.conversion_date)
-                .local()
-                .format('L')}
-            </div>
-            <div className="amount">{pendingConversion.amount}</div>{' '}
-          </div>
-        </CustomTooltip>
-      ))}
+      <div className="list-panel">
+        <div className="list">
+          {navParams.pendingConversions.map((pendingConversion: Conversion) => (
+            <CustomTooltip
+              dataTestId="tooltip-timesteamp-pending-conversion"
+              position="bottom"
+              key={pendingConversion.id}
+              message={chrome.i18n.getMessage(
+                'popup_html_pending_currency_timestamp',
+                [
+                  moment
+                    .utc(pendingConversion.conversion_date)
+                    .local()
+                    .format('L'),
+                  moment
+                    .utc(pendingConversion.conversion_date)
+                    .local()
+                    .format('LT'),
+                ],
+              )}
+              skipTranslation>
+              <div className="pending-conversion">
+                <div className="expiration-date">
+                  {moment
+                    .utc(pendingConversion.conversion_date)
+                    .local()
+                    .format('L')}
+                </div>
+                <div className="amount">{pendingConversion.amount}</div>{' '}
+              </div>
+            </CustomTooltip>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
