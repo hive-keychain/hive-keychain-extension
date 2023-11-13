c33a5292fb7229e861098a0e8b243b77d4458ac0
Cedric Gui <cedric.guillas78@gmail.com>
2023-11-13 09:13:43 +0800

    feat(survey): Designed survey popup

--- src/popup/hive/pages/app-container/survey/survey.component.tsx
+++ src/popup/hive/pages/app-container/survey/survey.component.tsx
@@ -3,6 +3,7 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import ButtonComponent, {
   ButtonType,
 } from 'src/common-ui/button/button.component';
+import { PopupContainer } from 'src/common-ui/popup-container/popup-container.component';
 import { Survey } from 'src/popup/hive/pages/app-container/survey/survey.interface';
 import { SurveyUtils } from 'src/popup/hive/utils/survey.utils';
 
@@ -36,35 +37,32 @@ const Survey = ({ survey }: Props) => {
   if (!ready) return null;
   else
     return (
-      <div data-testid="survey-component" className="survey">
-        <div className="overlay"></div>
-        <div className="survey-container">
-          <div className="survey-title">{survey.title}</div>
-          {image && (
-            <div className="image">
-              <img src={image.src} />
-            </div>
-          )}
-          <div className="description">
-            {survey.description.map((desc) => (
-              <p>{desc}</p>
-            ))}
+      <PopupContainer data-testid="survey-component" className="survey">
+        <div className="popup-title">{survey.title}</div>
+        {image && (
+          <div className="image">
+            <img src={image.src} />
           </div>
-          <div className="button-panel">
-            <ButtonComponent
-              type={ButtonType.ALTERNATIVE}
-              label="popup_html_survey_no_thanks"
-              onClick={() => noThanks()}
-            />
-            <ButtonComponent
-              dataTestId="help-us-page"
-              type={ButtonType.IMPORTANT}
-              label="popup_html_survey_help_us"
-              onClick={() => helpUs()}
-            />
-          </div>
+        )}
+        <div className="caption">
+          {survey.description.map((desc) => (
+            <div>{desc}</div>
+          ))}
         </div>
-      </div>
+        <div className="popup-footer">
+          <ButtonComponent
+            type={ButtonType.ALTERNATIVE}
+            label="popup_html_survey_no_thanks"
+            onClick={() => noThanks()}
+          />
+          <ButtonComponent
+            dataTestId="help-us-page"
+            type={ButtonType.IMPORTANT}
+            label="popup_html_survey_help_us"
+            onClick={() => helpUs()}
+          />
+        </div>
+      </PopupContainer>
     );
 };
 
