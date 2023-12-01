febee1644342a1b737600bbbbe83f16157c4f875
Cedric Gui <cedric.guillas78@gmail.com>
2023-12-01 15:19:15 +0800

    fix(ecosystem): Update class name

--- src/popup/hive/pages/app-container/home/ecosystem/ecosystem-category/ecosystem-category.component.tsx
+++ src/popup/hive/pages/app-container/home/ecosystem/ecosystem-category/ecosystem-category.component.tsx
@@ -24,7 +24,7 @@ export const EcosystemCategory = ({ category }: EcosystemCategoryProps) => {
   };
 
   return (
-    <div className="category">
+    <div className="ecosystem-category">
       {category.dapps.map((dapp, index) => (
         <div
           className="dapp"
