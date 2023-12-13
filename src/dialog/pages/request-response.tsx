fd544e3c0750f2db29528d3d44ffdae9f6bed9ad
Cedric Gui <cedric.guillas78@gmail.com>
2023-12-13 12:19:38 +0800

    fix(dialog): Error/success pages

--- src/dialog/pages/request-response.tsx
+++ src/dialog/pages/request-response.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ButtonComponent from 'src/common-ui/button/button.component';
-import DialogHeader from 'src/dialog/components/dialog-header/dialog-header.component';
+import { NewIcons } from 'src/common-ui/icons.enum';
+import { SVGIcon } from 'src/common-ui/svg-icon/svg-icon.component';
 
 type Props = {
   data: ResultMessage;
@@ -17,27 +18,55 @@ const RequestResponse = ({ data }: Props) => {
     }, 3000);
   }
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-      <DialogHeader
-        title={
-          data.msg.success
-            ? `${chrome.i18n.getMessage('dialog_header_success')} !`
-            : `${chrome.i18n.getMessage('dialog_header_error')} !`
-        }
-      />
-      {data.msg.message.split(/<br\s?\/?>/g).map((msg) => (
-        <div className="caption" style={{ wordBreak: 'break-word' }}>
-          {msg}
+    <div className="response-message-container">
+      <div className="message-card">
+        <SVGIcon
+          icon={
+            data.msg.success ? NewIcons.MESSAGE_SUCCESS : NewIcons.MESSAGE_ERROR
+          }
+        />
+        <div className="title">
+          {chrome.i18n.getMessage(
+            data.msg.success
+              ? 'message_container_title_success'
+              : 'message_container_title_fail',
+          )}
         </div>
-      ))}
-      <div className="fill-space" />
-      <ButtonComponent
-        label={'dialog_ok'}
-        onClick={() => {
-          window.close();
-        }}
-      />
+        <div className="message">
+          {data.msg.message.split(/<br\s?\/?>/g).map((msg, index) => (
+            <p key={`p-${index}`} style={{ wordBreak: 'break-word' }}>
+              {msg}
+            </p>
+          ))}
+        </div>
+        <ButtonComponent
+          label="message_container_close_button"
+          onClick={close}
+        />
+      </div>
     </div>
+
+    // <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+    //   <DialogHeader
+    //     title={
+    //       data.msg.success
+    //         ? `${chrome.i18n.getMessage('dialog_header_success')} !`
+    //         : `${chrome.i18n.getMessage('dialog_header_error')} !`
+    //     }
+    //   />
+    //   {data.msg.message.split(/<br\s?\/?>/g).map((msg) => (
+    //     <div className="caption" style={{ wordBreak: 'break-word' }}>
+    //       {msg}
+    //     </div>
+    //   ))}
+    //   <div className="fill-space" />
+    //   <ButtonComponent
+    //     label={'dialog_ok'}
+    //     onClick={() => {
+    //       window.close();
+    //     }}
+    //   />
+    // </div>
   );
 };
 
