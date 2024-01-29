87ffc2233af9ffa73bb5dfc13ccab7087e3ee28d
theghost1980 <saturnob612@gmail.com>
2024-01-29 16:10:59 -0400

    update: mixed as one data: tokenBalance + ext acc, IP:SR

--- src/portfolio/portfolio.component.tsx
+++ src/portfolio/portfolio.component.tsx
@@ -1,9 +1,13 @@
 import { ExtendedAccount } from '@hiveio/dhive';
+import { CurrencyPrices } from '@interfaces/bittrex.interface';
 import { GlobalProperties } from '@interfaces/global-properties.interface';
 import { LocalAccount } from '@interfaces/local-account.interface';
+import { TokenBalance } from '@interfaces/tokens.interface';
 import AccountUtils from '@popup/hive/utils/account.utils';
+import CurrencyPricesUtils from '@popup/hive/utils/currency-prices.utils';
 import { DynamicGlobalPropertiesUtils } from '@popup/hive/utils/dynamic-global-properties.utils';
 import HiveUtils from '@popup/hive/utils/hive.utils';
+import TokensUtils from '@popup/hive/utils/tokens.utils';
 import { Theme } from '@popup/theme.context';
 import { LocalStorageKeyEnum } from '@reference-data/local-storage-key.enum';
 import { getTheme } from '@table-library/react-table-library/baseline';
@@ -17,159 +21,7 @@ import { SVGIcon } from 'src/common-ui/svg-icon/svg-icon.component';
 import FormatUtils from 'src/utils/format.utils';
 import LocalStorageUtils from 'src/utils/localStorage.utils';
 //TODO important each 0 value, place a - no value. Tokens & currencies.
-
-const nodes = [
-  {
-    id: '0',
-    name: 'Shopping List',
-    deadline: 'dedaline',
-    type: 'TASK',
-    isComplete: true,
-    nodes: 3,
-  },
-  {
-    id: '1',
-    name: 'Shopping List',
-    deadline: 'dedaline',
-    type: 'TASK',
-    isComplete: true,
-    nodes: 3,
-  },
-  {
-    id: '2',
-    name: 'Shopping List',
-    deadline: 'dedaline',
-    type: 'TASK',
-    isComplete: true,
-    nodes: 3,
-  },
-  {
-    id: '3',
-    name: 'Shopping List',
-    deadline: 'dedaline',
-    type: 'TASK',
-    isComplete: true,
-    nodes: 3,
-  },
-  {
-    id: '4',
-    name: 'Shopping List',
-    deadline: 'dedaline',
-    type: 'TASK',
-    isComplete: true,
-    nodes: 3,
-  },
-  {
-    id: '5',
-    name: 'Shopping List',
-    deadline: 'dedaline',
-    type: 'TASK',
-    isComplete: true,
-    nodes: 3,
-  },
-  {
-    id: '5',
-    name: 'Shopping List',
-    deadline: 'dedaline',
-    type: 'TASK',
-    isComplete: true,
-    nodes: 3,
-  },
-  {
-    id: '6',
-    name: 'Shopping List',
-    deadline: 'dedaline',
-    type: 'TASK',
-    isComplete: true,
-    nodes: 3,
-  },
-  {
-    id: '7',
-    name: 'Shopping List',
-    deadline: 'dedaline',
-    type: 'TASK',
-    isComplete: true,
-    nodes: 3,
-  },
-  {
-    id: '8',
-    name: 'Shopping List',
-    deadline: 'dedaline',
-    type: 'TASK',
-    isComplete: true,
-    nodes: 3,
-  },
-  {
-    id: '9',
-    name: 'Shopping List',
-    deadline: 'dedaline',
-    type: 'TASK',
-    isComplete: true,
-    nodes: 3,
-  },
-  {
-    id: '9',
-    name: 'Shopping List',
-    deadline: 'dedaline',
-    type: 'TASK',
-    isComplete: true,
-    nodes: 3,
-  },
-  {
-    id: '10',
-    name: 'Shopping List',
-    deadline: 'dedaline',
-    type: 'TASK',
-    isComplete: true,
-    nodes: 3,
-  },
-  {
-    id: '11',
-    name: 'Shopping List',
-    deadline: 'dedaline',
-    type: 'TASK',
-    isComplete: true,
-    nodes: 3,
-  },
-  {
-    id: '12',
-    name: 'Shopping List',
-    deadline: 'dedaline',
-    type: 'TASK',
-    isComplete: true,
-    nodes: 3,
-  },
-  {
-    id: '13',
-    name: 'Shopping List',
-    deadline: 'dedaline',
-    type: 'TASK',
-    isComplete: true,
-    nodes: 3,
-  },
-  {
-    id: '14',
-    name: 'Shopping List',
-    deadline: 'dedaline',
-    type: 'TASK',
-    isComplete: true,
-    nodes: 3,
-  },
-];
-
-const COLUMNS = [
-  { label: 'Task', renderCell: (item: any) => item.name },
-  {
-    label: 'Deadline',
-    renderCell: (item: any) => item.deadline,
-  },
-  { label: 'Type', renderCell: (item: any) => item.type },
-  {
-    label: 'Complete',
-    renderCell: (item: any) => item.isComplete.toString(),
-  },
-  { label: 'Tasks', renderCell: (item: any) => item.nodes },
-];
+//TODO when finished, refactor code as possible, adding utils, etc.
 
 const PortfolioComponent = () => {
   const [theme, setTheme] = useState<Theme>();
@@ -180,42 +32,19 @@ const PortfolioComponent = () => {
   const [globalProperties, setGlobalProperties] =
     useState<GlobalProperties | null>(null);
   const [isLoading, setIsLoading] = useState(true);
-  // const [columns, setColumns] = useState([]); //TODO see if types needed at all
-  const [data, setData] = useState<{ nodes: ExtendedAccount[] }>({ nodes: [] });
+
+  //TODO bellow needs to define properly in order to have ExtendedAccount Data + TokenBalance, somehow.
+  const [data, setData] = useState<{ nodes: any[] }>({ nodes: [] });
+
   const [extraColumns, setExtraColumns] = useState<
     { label: string; renderCell: (item: any) => void }[]
   >([]);
   const [themeTable, setThemeTable] = useState<any>();
-
-  const getCOLUMNS = (globalProperties: GlobalProperties) => [
-    { label: 'Account', renderCell: (item: ExtendedAccount) => item.name },
-    {
-      label: 'HP',
-      renderCell: (item: ExtendedAccount) =>
-        FormatUtils.withCommas(
-          FormatUtils.toHP(
-            item.vesting_shares as string,
-            globalProperties.globals,
-          ).toFixed(6),
-        ),
-    },
-    { label: 'HIVE', renderCell: (item: ExtendedAccount) => item.balance },
-    {
-      label: 'HBD',
-      renderCell: (item: ExtendedAccount) => item.hbd_balance,
-    },
-  ];
-
-  //testing data react tables
-  // const themeTable = useTheme([
-  //   getTheme(),
-  //   {
-  //     Table: `
-  //        --data-table-library_grid-template-columns:  25% 25% 25% 25% 25% minmax(150px, 1fr);
-  //     `,
-  //   },
-  // ]);
-  // const data = { nodes };
+  const [currencyPrices, setCurrencyPrices] = useState<CurrencyPrices>();
+  // let tokensBalanceList: TokenBalance[][] = [];
+  const [tokensBalanceList, setTokensBalanceList] = useState<TokenBalance[][]>(
+    [],
+  );
 
   useEffect(() => {
     init();
@@ -240,7 +69,22 @@ const PortfolioComponent = () => {
       setExtendedAccountsList(extAccounts);
       console.log({ extAccounts }); //TODO remove line
       loadGlobalProps();
-      setIsLoading(false);
+      loadCurrencyPrices();
+      loadUserTokens(localAccounts);
+      // setIsLoading(false);
+    }
+  };
+
+  const loadCurrencyPrices = async () => {
+    try {
+      const prices = await CurrencyPricesUtils.getPrices();
+      if (prices) {
+        setCurrencyPrices(prices);
+      }
+      console.log({ prices });
+    } catch (error) {
+      //TODO add as Logger.
+      console.log('Error loading prices!', { error });
     }
   };
 
@@ -274,8 +118,47 @@ const PortfolioComponent = () => {
     }
   };
 
+  const getFormatedValue = (value: string, key: string) => {
+    switch (true) {
+      case value.includes('VESTS'):
+        return FormatUtils.withCommas(
+          FormatUtils.toHP(
+            (value as string).split(' ')[0],
+            globalProperties?.globals,
+          ).toString(),
+        );
+      case value.includes('HBD'):
+        return FormatUtils.withCommas((value as string).split(' ')[0]);
+      case value.includes('HIVE'):
+        return FormatUtils.withCommas((value as string).split(' ')[0]);
+      default:
+        return value;
+    }
+  };
+
   const getRenderCell = (item: any, key: string) => {
+    //TODO bellow check & add missing keys per item/object/div
+    //TODO add to translations if needed or check if already there.
+    if (!item[key]) return <div>-</div>;
+
     if (key === 'name') {
+      if (item[key] === 'totals')
+        return (
+          <div
+            className="avatar-username-container"
+            key={'totals-last-row-table'}>
+            <div className="account-name">TOTALS</div>
+          </div>
+        );
+      if (item[key] === 'totals_usd') {
+        return (
+          <div
+            className="avatar-username-container"
+            key={'totals-last-row-table'}>
+            <div className="account-name">USD VALUE</div>
+          </div>
+        );
+      }
       return (
         <div className="avatar-username-container">
           <PreloadedImage
@@ -288,43 +171,152 @@ const PortfolioComponent = () => {
         </div>
       );
     } else {
-      return String(item[key]);
+      const formatedValue = getFormatedValue(String(item[key]), key);
+      return <div className="text">{formatedValue}</div>;
     }
   };
 
   //DATA set
+  const getTotal = (
+    key: 'balance' | 'hbd_balance' | 'vesting_shares',
+    list: ExtendedAccount[],
+  ) => {
+    if (key === 'balance') {
+      return (
+        list
+          .reduce(
+            (acc, curr) => acc + Number((curr.balance as string).split(' ')[0]),
+            0,
+          )
+          .toString() + ' HIVE'
+      );
+    } else if (key === 'hbd_balance') {
+      return (
+        list
+          .reduce(
+            (acc, curr) =>
+              acc + Number((curr.hbd_balance as string).split(' ')[0]),
+            0,
+          )
+          .toString() + ' HBD'
+      );
+    } else {
+      //(key === 'vesting_shares')
+      return (
+        list
+          .reduce(
+            (acc, curr) =>
+              acc + Number((curr.vesting_shares as string).split(' ')[0]),
+            0,
+          )
+          .toString() + ' VESTS'
+      );
+    }
+  };
+
+  const loadUserTokens = async (accountNames: LocalAccount[]) => {
+    //Get tokenBalance list
+    let tempTokenBalanceList: TokenBalance[][] = [];
+    for (let index = 0; index < accountNames.length; index++) {
+      const accountName = accountNames[index].name;
+      let tokensBalance: TokenBalance[] = await TokensUtils.getUserBalance(
+        accountName,
+      );
+      tokensBalance = tokensBalance.sort(
+        (a, b) => parseFloat(b.balance) - parseFloat(a.balance),
+      );
+      tempTokenBalanceList.push(tokensBalance);
+    }
+    //sort list who has more tokens
+    tempTokenBalanceList = tempTokenBalanceList.sort(
+      (a, b) => b.length - a.length,
+    );
+    setTokensBalanceList(tempTokenBalanceList);
+  };
+
   useEffect(() => {
-    if (extendedAccountsList.length > 0 && globalProperties?.globals) {
-      // setColumns([
-      //   { label: 'Account', renderCell: (item: ExtendedAccount) => item.name },
-      //   {
-      //     label: 'HP',
-      //     renderCell: (item: ExtendedAccount) =>
-      //       FormatUtils.withCommas(
-      //         FormatUtils.toHP(
-      //           item.vesting_shares as string,
-      //           globalProperties.globals,
-      //         ).toFixed(6),
-      //       ),
-      //   },
-      //   { label: 'HIVE', renderCell: (item: ExtendedAccount) => item.balance },
-      //   {
-      //     label: 'HBD',
-      //     renderCell: (item: ExtendedAccount) => item.hbd_balance,
-      //   },
-      // ]);
-      const keysToUse = ['name', 'vesting_shares', 'hbd_balance', 'balance'];
-      setData({
-        nodes: extendedAccountsList,
-      });
-      const extra = Object.keys(extendedAccountsList[0])
-        .filter((k) => keysToUse.includes(k))
-        .map((key) => {
+    if (
+      extendedAccountsList.length > 0 &&
+      globalProperties?.globals &&
+      currencyPrices &&
+      tokensBalanceList.length > 0
+    ) {
+      //TODO next step bellow.
+      //  - getTokens
+      //  - getUserBalance of each account
+      //  - select the account who has more tokens. Using this symbols as array[strings], add them into keysToUse
+      //  - to  test, add the new item value as 0.
+      //  - render each token balance of each user in their column.
+      //  - apply the - considering the renderItem function.
+      //TODO keep working on this
+
+      const tokensSymbols = tokensBalanceList[0].map((token) => token.symbol);
+      console.log({ tokensSymbols });
+      const totalBalance = getTotal('balance', extendedAccountsList);
+      const totalHBDBalance = getTotal('hbd_balance', extendedAccountsList);
+      const totalHP = getTotal('vesting_shares', extendedAccountsList);
+
+      const portfolioUserData = extendedAccountsList.map(
+        ({ name, balance, vesting_shares, hbd_balance }) => {
+          const tokenBalance = tokensBalanceList.find(
+            (tokenBalance) => tokenBalance[0].account === name,
+          );
+          const tokenBalanceObj = tokenBalance?.map(({ symbol, balance }) => {
+            return { [symbol]: balance };
+          });
+          let asPlainObjects: { [key: string]: any } = {};
+          for (const [key, value] of Object.entries(tokenBalanceObj!)) {
+            asPlainObjects = { ...asPlainObjects, ...value };
+          }
           return {
-            label: getLabelCell(key),
-            renderCell: (item: any) => getRenderCell(item, key),
+            name,
+            balance,
+            vesting_shares,
+            hbd_balance,
+            ...asPlainObjects,
           };
-        });
+        },
+      );
+
+      console.log({ portfolioUserData }); //TODO remove line
+      const keysToUse = Object.keys(portfolioUserData[0]).map((key) => key);
+      setData({
+        nodes: [
+          ...portfolioUserData,
+          {
+            name: 'totals',
+            balance: totalBalance,
+            hbd_balance: totalHBDBalance,
+            vesting_shares: totalHP,
+          } as any,
+          {
+            name: 'totals_usd',
+            balance: FormatUtils.formatCurrencyValue(
+              +totalBalance.split(' ')[0]! * (currencyPrices.hive.usd ?? 1),
+            ),
+            hbd_balance: FormatUtils.formatCurrencyValue(
+              +totalHBDBalance.split(' ')[0] *
+                (currencyPrices.hive_dollar.usd ?? 1),
+            ),
+            vesting_shares: FormatUtils.formatCurrencyValue(
+              +FormatUtils.toHP(
+                getTotal('vesting_shares', extendedAccountsList),
+                globalProperties.globals,
+              ) * (currencyPrices.hive.usd ?? 1),
+            ),
+          } as any,
+        ],
+      });
+      // const extendedAccountFilteredKeys = Object.keys(
+      //   extendedAccountsList[0],
+      // ).filter((k) => keysToUse.includes(k));
+      // const tokensColumns = ['LEO'];
+      const extra = keysToUse.map((key) => {
+        return {
+          label: getLabelCell(key),
+          renderCell: (item: any) => getRenderCell(item, key),
+        };
+      });
       setExtraColumns(extra);
       const dynamicPercentages = keysToUse
         .slice(0, keysToUse.length - 1)
@@ -339,6 +331,8 @@ const PortfolioComponent = () => {
           `,
             HeaderRow: `
           background-color: ${theme === Theme.DARK ? '#293144' : '#fafcfd'};
+          color: var(--main-font-color);
+          @include poppins500(14px);
         `,
             Row: `
           &:nth-of-type(odd) {
@@ -354,7 +348,7 @@ const PortfolioComponent = () => {
       );
       setIsLoading(false);
     }
-  }, [extendedAccountsList, globalProperties]);
+  }, [extendedAccountsList, globalProperties, tokensBalanceList]);
 
   return (
     <div className={`theme ${theme} portfolio`}>
@@ -367,14 +361,25 @@ const PortfolioComponent = () => {
           <RotatingLogoComponent />
         </div>
       )}
-      {!isLoading && data && globalProperties && extraColumns && (
-        <CompactTable
-          columns={extraColumns}
-          data={data}
-          theme={themeTable}
-          layout={{ horizontalScroll: true, fixedHeader: true, custom: true }}
-        />
-      )}
+      {!isLoading &&
+        data &&
+        globalProperties &&
+        extraColumns &&
+        currencyPrices &&
+        tokensBalanceList.length > 0 && (
+          <>
+            <CompactTable
+              columns={extraColumns}
+              data={data}
+              theme={themeTable}
+              layout={{
+                horizontalScroll: true,
+                fixedHeader: true,
+                custom: true,
+              }}
+            />
+          </>
+        )}
     </div>
   );
 };
