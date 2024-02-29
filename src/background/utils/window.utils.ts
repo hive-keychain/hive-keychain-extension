7100569584941b51b1465a56ab7473062972e5a9
stoodkev <quentin.correa@gmail.com>
2024-02-29 12:42:01 +0800

    window.utils.ts

---
+++ src/background/utils/window.utils.ts
@@ -0,0 +1,32 @@
+import { DialogCommand } from '@reference-data/dialog-message-key.enum';
+
+/* istanbul ignore next */
+export const waitUntilDialogIsReady = async (
+  ms: number,
+  command: DialogCommand,
+  callback: () => void,
+  nb = 0,
+) => {
+  nb++;
+  if (await askIfReady(ms, command)) {
+    callback();
+  } else {
+    waitUntilDialogIsReady(ms, command, callback, nb);
+  }
+};
+/* istanbul ignore next */
+const askIfReady = (ms: number, command: DialogCommand) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(false);
+    }, ms);
+    chrome.runtime.sendMessage(
+      {
+        command,
+      },
+      (resp) => {
+        if (resp) resolve(resp);
+      },
+    );
+  });
+};
