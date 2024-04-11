c8a912574ee823f381179142ae2f76ba7c085445
theghost1980 <saturnob612@gmail.com>
2024-04-11 17:06:46 -0400

    feature(tutorial): added data ref

---
+++ src/reference-data/tutorial.ts
@@ -0,0 +1,71 @@
+export const tutorialSlideDataList = [
+  {
+    title: 'What is Keychain?',
+    paragraphs: [
+      "Keychain is your go-to solution for navigating the Hive ecosystem with ease and security, right at your fingertips. Whether you're on a computer or the go with your smartphone, Keychain empowers you to manage your Hive accounts effortlessly in a way you won't worry about exposing your private keys. With Hive Keychain, you can transfer tokens, cast votes, and handle a variety of transactions seamlessly.",
+      'Simple to use and designed with your safety in mind, Keychain is the perfect companion for anyone looking to make the most out of their Hive experience.',
+    ],
+    images: [],
+  },
+  {
+    title: 'Four Keys but only three are needed. Why?',
+    description:
+      'Each Hive key has its own purpose in regards of your account security.',
+    list: [
+      'Switch between accounts.',
+      'Check your Voting Power and Resources.',
+      'Access and manage your Hive native tokens and Hive Engine tokens.',
+      'Discover the Ecosystem, Send, Buy, and Swap tokens. More on this later',
+      'Access the Settings Menu in the upper left corner.',
+    ],
+  },
+  {
+    title: 'Hive, HBD, and Hive Power. What are the differences?',
+    description:
+      "Description: Hive and HBD are the native tokens of the Hive blockchain. Hive can be stored in its liquid form called Hive, and its vested form called Hive Power. Here's more information:",
+    list: [
+      'Hive: Main token of the Hive blockchain, used for transactions and trades.',
+      'Hive Power (HP): Vested Hive. Having more HP increases your voting weight, your resource credits and your influence in matters of governance.',
+      'HBD (Hive Backed Dollars): Stablecoin pegged to the US dollar, used for stable transactions. HBD can be locked in savings for a {{savings_apr}}% APR.',
+    ],
+    finalLine: 'Hive Engine tokens are also called layer 2 tokens.',
+  },
+  {
+    title: 'Why do I need Voting Mana and Resource Credits?',
+    paragraphs: [
+      'Voting power (VP) determines how much your vote influences the distribution of rewards on the platform through votes. Voting mana (VM) will decrease when you vote for posts or comments and reduce your VP.',
+      'Resource Credits (RC) define the ability to perform actions on the blockchain, such as transferring tokens or publishing.',
+      'Both VM and RC are recharged at a rate of 20% per day.',
+      'The more Hive Power you have, the higher your VP gets and the more RC you have.',
+    ],
+  },
+  {
+    title: 'Fast and Feeless Transactions with Hive Keychain',
+    paragraphs: [
+      "Click on the 'Send' button to transfer Hive or HBD. Just fill in the username, the amount, and the memo (optional). If needed, you can also encrypt the memo by clicking on the encryption icon, or start recurrent transfers. All transactions are fast and free, just make sure you have enough RC.",
+    ],
+    finalLine:
+      'As a reminder, on Hive the wallet address is the username itself.',
+  },
+  {
+    title: 'Accessing dApps in the simplest way',
+    paragraphs: [
+      '<On extension> Explore the Keychain Ecosystem tab, on the bottom left of the extension, and find your new favorite dApps!',
+      'Interacting with Hive dApps has never been easier! Simply use the Hive Keychain option within your favorite platform and enter your username. Keychain will ask your confirmation before executing the transaction. For a smoother experience, you can whitelist some operations that do not affect your balance (ie. posting, voting). These will be confirmed automatically.',
+      '<On mobile> Explore the Keychain Ecosystem tab, on the brower section of the App, and find your new favorite dApps!',
+      'Interacting with Hive dApps has never been easier! Simply open the in-App browser, use the Hive Keychain option within your favorite dApp and enter your username. Keychain will then ask your confirmation before signing transactions. For a smoother experience, you can whitelist some operations that do not affect your balance (ie. posting, voting). These will be confirmed automatically.',
+    ],
+  },
+  {
+    title: 'Swapped all!',
+    description:
+      'There is no better way to make swaps than with Hive Keychain. In a few clicks you can swap between native Hive tokens and Hive Engine tokens. Instead of making several transactions, sit back and let Hive Keychain take care of it for you. You can follow the progress ont the swap history page.',
+  },
+  {
+    title: 'Buy Hive and HBD easily!',
+    paragraphs: [
+      "<On extension> Buying Hive and HBD through Keychain is super easy. Just head to the Buy tab to see all available options, for both Hive and HBD. In each case, you should follow the steps required by each option to make the purchase, and the tokens will arrive in your Hive account without any issues. It's worth noting that with Transak, you can buy Hive using fiat directly through credit cards, bank transfers, Apple Pay, and more.\n</On extension>",
+      "<On mobile> Buying Hive and HBD through Keychain is super easy. Just head to the Swap button to see all available options, for both Hive and HBD. In each case, you should follow the steps required by each option to make the purchase, and the tokens will arrive in your Hive account without any issues. It's worth noting that with Transak, you can buy Hive using fiat directly through credit cards, bank transfers, Apple Pay, and more.\n</On mobile>",
+    ],
+  },
+];
