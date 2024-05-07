bf72977af813d790c0874e2a844f10458782a178
theghost1980 <saturnob612@gmail.com>
2024-05-07 07:13:30 -0400

    update(swap-widget): added webpack, react, ts init config

---
+++ swap-widget/app.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+function App() {
+  return <div>{'react setup from scratch without cra testing ts'}</div>;
+}
+export default App;
---
+++ swap-widget/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  mode: 'development',
+  entry: './main.tsx',
+  devtool: 'inline-source-map',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+};
