65bbc431b135586d02830acf2662dae8dee70f82
theghost1980 <saturnob612@gmail.com>
2024-05-07 11:28:01 -0400

    update(swap-widget): IP: load ext modules, SR

--- swap-widget/app.tsx
+++ swap-widget/app.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
+import { SwapTokenUtils } from '@extension/utils/swap-token.utils';
+import React, { useEffect } from 'react';
 function App() {
+  useEffect(() => {
+    SwapTokenUtils.getServerStatus().then((res) => {
+      console.log({ res });
+    });
+  }, []);
+
   return <div>{'react setup from scratch without cra testing ts'}</div>;
 }
 export default App;
--- swap-widget/webpack.config.js
+++ swap-widget/webpack.config.js
@@ -22,6 +22,7 @@ module.exports = {
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
+        include: [__dirname, path.resolve(__dirname, '../src')],
         exclude: /node_modules/,
       },
     ],
