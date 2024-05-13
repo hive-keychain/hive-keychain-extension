19f18e0fa9aea68b51d39fc67d29984615b58c06
Cedric Gui <cedric.guillas78@gmail.com>
2024-05-13 09:25:07 +0800

    feat(evm transfer): created gas fee panel

--- src/popup/evm/pages/home/gas-fee-panel/gas-fee-panel.component.tsx
+++ src/popup/evm/pages/home/gas-fee-panel/gas-fee-panel.component.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { PopupContainer } from 'src/common-ui/popup-container/popup-container.component';
+import { Separator } from 'src/common-ui/separator/separator.component';
 
 export const GasFeePanel = () => {
   const [isPanelOpen, setIsPanelOpen] = useState(false);
@@ -28,7 +29,59 @@ export const GasFeePanel = () => {
           </div>
         </div>
       </div>
-      {isPanelOpen && <PopupContainer children={'Popup'} />}
+      {isPanelOpen && (
+        <PopupContainer
+          children={
+            <div className="edit-gas-fee-content">
+              <div className="title">
+                {chrome.i18n.getMessage('popup_html_evm_edit_gas_fee')}
+              </div>
+              <Separator fullSize type="horizontal" />
+              <div className="custom-fee-row low">
+                <div className="label type">
+                  {chrome.i18n.getMessage('popup_html_evm_custom_gas_fee_low')}
+                </div>
+                <div className="label duration">
+                  {chrome.i18n.getMessage(
+                    'popup_html_evm_gas_fee_estimate_duration',
+                    ['30'],
+                  )}
+                </div>
+                <div className="label gas-fee">0.000001</div>
+              </div>
+
+              <div className="custom-fee-row medium">
+                <div className="label type">
+                  {chrome.i18n.getMessage(
+                    'popup_html_evm_custom_gas_fee_medium',
+                  )}
+                </div>
+                <div className="label duration">
+                  {chrome.i18n.getMessage(
+                    'popup_html_evm_gas_fee_estimate_duration',
+                    ['30'],
+                  )}
+                </div>
+                <div className="label gas-fee">0.000001</div>
+              </div>
+              <div className="custom-fee-row aggressive">
+                <div className="label type">
+                  {chrome.i18n.getMessage(
+                    'popup_html_evm_custom_gas_fee_aggresive',
+                  )}
+                </div>
+                <div className="label duration">
+                  {chrome.i18n.getMessage(
+                    'popup_html_evm_gas_fee_estimate_duration',
+                    ['30'],
+                  )}
+                </div>
+                <div className="label gas-fee">0.000001</div>
+              </div>
+            </div>
+          }
+        />
+      )}
     </>
   );
 };
