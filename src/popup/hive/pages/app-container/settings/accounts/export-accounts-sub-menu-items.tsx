b521cc745b967bff6b3dd4041f7c23cc5a9d5324
theghost1980 <saturnob612@gmail.com>
2024-05-30 12:47:07 -0400

    update(export-qr-accounts): added basic sub menu, icon, tr. IP:SR

---
+++ src/popup/hive/pages/app-container/settings/accounts/export-accounts-sub-menu-items.tsx
@@ -0,0 +1,26 @@
+import { MenuItem } from '@interfaces/menu-item.interface';
+import AccountUtils from '@popup/hive/utils/account.utils';
+import { store } from '@popup/multichain/store';
+import { SVGIcons } from 'src/common-ui/icons.enum';
+
+const ExportAccountsSubMenuItems: MenuItem[] = [
+  {
+    label: 'popup_html_export',
+    icon: SVGIcons.MENU_ACCOUNTS_EXPORT,
+    action: () => {
+      AccountUtils.downloadAccounts(
+        store.getState().hive.accounts,
+        store.getState().mk,
+      );
+    },
+  },
+  {
+    label: 'popup_html_export_all_accounts_as_QR',
+    icon: SVGIcons.MENU_EXPORT_ACCOUNTS_QR,
+    action: () => {
+      console.log('//TODO QR generation');
+    },
+  },
+];
+
+export default ExportAccountsSubMenuItems;
---
+++ src/popup/hive/pages/app-container/settings/accounts/export-accounts/export-accounts-sub-menu.component.tsx
@@ -0,0 +1,29 @@
+import ExportAccountsSubMenuItems from '@popup/hive/pages/app-container/settings/accounts/export-accounts-sub-menu-items';
+import { RootState } from '@popup/multichain/store';
+import { Screen } from '@reference-data/screen.enum';
+import React from 'react';
+import { ConnectedProps, connect } from 'react-redux';
+import { MenuComponent } from 'src/common-ui/menu/menu.component';
+
+const ExportAccountsSubMenu = ({}: PropsFromRedux) => {
+  return (
+    <div
+      className="settings-manage-account-authorities"
+      data-testid={`${Screen.SETTINGS_EXPORT_ACCOUNTS}-page`}>
+      <MenuComponent
+        title="popup_html_export_accounts"
+        isBackButtonEnable={true}
+        menuItems={ExportAccountsSubMenuItems}></MenuComponent>
+      //TODO
+    </div>
+  );
+};
+
+const mapStateToProps = (state: RootState) => {
+  return {};
+};
+
+const connector = connect(mapStateToProps, {});
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+export const ExportAccountsSubMenuComponent = connector(ExportAccountsSubMenu);
