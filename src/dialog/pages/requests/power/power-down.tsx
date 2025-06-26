08c9b5eaa600166faf99ce733e15e16b8c7968ef
carlodizon03 <carlodizon03@gmail.com>
2025-06-26 10:32:01 +0800

    usernameWithAvatar in power down/up

--- src/dialog/pages/requests/power/power-down.tsx
+++ src/dialog/pages/requests/power/power-down.tsx
@@ -2,6 +2,7 @@ import { RequestId, RequestPowerDown } from '@interfaces/keychain.interface';
 import { Rpc } from '@interfaces/rpc.interface';
 import React from 'react';
 import { Separator } from 'src/common-ui/separator/separator.component';
+import UsernameWithAvatar from 'src/common-ui/username-with-avatar/username-with-avatar';
 import Operation from 'src/dialog/components/operation/operation';
 import RequestItem from 'src/dialog/components/request-item/request-item';
 import CurrencyUtils from 'src/popup/hive/utils/currency.utils';
@@ -21,7 +22,7 @@ const PowerDown = (props: Props) => {
     <Operation
       title={chrome.i18n.getMessage('dialog_title_powerdown')}
       {...props}>
-      <RequestItem title="dialog_account" content={`@${data.username}`} />
+      <UsernameWithAvatar title="dialog_account" username={data.username} />
       <Separator type={'horizontal'} fullSize />
       <RequestItem
         title="dialog_amount"
--- src/dialog/pages/requests/power/power-up.tsx
+++ src/dialog/pages/requests/power/power-up.tsx
@@ -2,6 +2,7 @@ import { RequestId, RequestPowerUp } from '@interfaces/keychain.interface';
 import { Rpc } from '@interfaces/rpc.interface';
 import React from 'react';
 import { Separator } from 'src/common-ui/separator/separator.component';
+import UsernameWithAvatar from 'src/common-ui/username-with-avatar/username-with-avatar';
 import Operation from 'src/dialog/components/operation/operation';
 import RequestItem from 'src/dialog/components/request-item/request-item';
 import CurrencyUtils from 'src/popup/hive/utils/currency.utils';
@@ -21,9 +22,9 @@ const PowerUp = (props: Props) => {
     <Operation
       title={chrome.i18n.getMessage('dialog_title_powerup')}
       {...props}>
-      <RequestItem title="dialog_account" content={`@${data.username}`} />
+      <UsernameWithAvatar title="dialog_account" username={data.username} />
       <Separator type={'horizontal'} fullSize />
-      <RequestItem title="dialog_to" content={`@${data.recipient}`} />
+      <UsernameWithAvatar title="dialog_to" username={data.recipient} />
       <Separator type={'horizontal'} fullSize />
       <RequestItem
         title="dialog_amount"
