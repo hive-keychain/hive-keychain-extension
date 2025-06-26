6d9ca52807ab472c896c7374a621ca1488504850
carlodizon03 <carlodizon03@gmail.com>
2025-06-26 11:10:39 +0800

    usernameWithAvatar in delegateion

--- src/dialog/pages/requests/delegation.tsx
+++ src/dialog/pages/requests/delegation.tsx
@@ -2,6 +2,7 @@ import { RequestDelegation, RequestId } from '@interfaces/keychain.interface';
 import { Rpc } from '@interfaces/rpc.interface';
 import React from 'react';
 import { Separator } from 'src/common-ui/separator/separator.component';
+import UsernameWithAvatar from 'src/common-ui/username-with-avatar/username-with-avatar';
 import Operation from 'src/dialog/components/operation/operation';
 import RequestItem from 'src/dialog/components/request-item/request-item';
 import { useAnonymousRequest } from 'src/dialog/hooks/anonymous-requests';
@@ -20,8 +21,8 @@ const Delegation = (props: Props) => {
   const { data, accounts, rpc } = props;
   const anonymousProps = useAnonymousRequest(data, accounts);
   const renderUsername = () => {
-    return !accounts ? (
-      <RequestItem title={'dialog_account'} content={`@${data.username}`} />
+    return !accounts && data.username ? (
+      <UsernameWithAvatar title={'dialog_account'} username={data.username} />
     ) : (
       <></>
     );
@@ -33,7 +34,7 @@ const Delegation = (props: Props) => {
       {...anonymousProps}>
       {renderUsername()}
       <Separator type={'horizontal'} fullSize />
-      <RequestItem title="dialog_delegatee" content={`@${data.delegatee}`} />
+      <UsernameWithAvatar title="dialog_delegatee" username={data.delegatee} />
       <Separator type={'horizontal'} fullSize />
       <RequestItem
         title="dialog_amount"
