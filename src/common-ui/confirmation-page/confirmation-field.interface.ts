feecfe1ed912b97b56129ef4ac7b8a6ff06a1fc6
carlodizon03 <carlodizon03@gmail.com>
2025-06-28 21:07:54 +0800

    feature(improve-confirmation-86etqnjy0): confirmation page tag field

--- src/common-ui/confirmation-page/confirmation-field.interface.ts
+++ src/common-ui/confirmation-page/confirmation-field.interface.ts
@@ -4,4 +4,13 @@ export interface ConfirmationPageFields {
   labelParams?: string[];
   valueParams?: string[];
   valueClassName?: string;
+  tag?: ConfirmationPageFieldTag;
+  tokenSymbol?: string;
+}
+
+export enum ConfirmationPageFieldTag {
+  USERNAME = 'username',
+  BALANCE = 'balance',
+  AMOUNT = 'amount',
+  OPERATION_TYPE = 'operation_type',
 }
