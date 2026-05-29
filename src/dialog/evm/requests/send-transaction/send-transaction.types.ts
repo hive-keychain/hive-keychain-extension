aa4dc514dad695efa405a8b6fcbcd35bada11673
stoodkev <quentin.correa@gmail.com>
2026-05-29 15:44:06 +0800

    fix(ts): resolve ts issues for prod build

--- src/dialog/evm/requests/send-transaction/send-transaction.types.ts
+++ src/dialog/evm/requests/send-transaction/send-transaction.types.ts
@@ -19,6 +19,7 @@ export type SendTransactionHookApi = Pick<
   | 'initPendingTransactionWarning'
   | 'setUnableToReachBackend'
   | 'hydrateDomainFieldWarnings'
+  | 'setErrorMessage'
 >;
 
 export interface SendTransactionInitSetters {
