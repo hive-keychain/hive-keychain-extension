bd28f6f4d1ff7971b2571417859a04ec44f3aa5e
stoodkev <quentin.correa@gmail.com>
2026-07-22 09:09:05 +0200

    fix(accessibility): make back-to-top keyboard accessible

--- src/common-ui/back-to-top-button/back-to-top-button.component.tsx
+++ src/common-ui/back-to-top-button/back-to-top-button.component.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { SVGIcons } from 'src/common-ui/icons.enum';
 import { SVGIcon } from 'src/common-ui/svg-icon/svg-icon.component';
+import { I18nUtils } from 'src/utils/i18n.utils';
 
 interface BackToTopButtonProps {
-  element: any;
+  element: React.RefObject<HTMLElement>;
 }
 
 export const BackToTopButton = (props: BackToTopButtonProps) => {
@@ -14,8 +15,12 @@ export const BackToTopButton = (props: BackToTopButtonProps) => {
   };
 
   return (
-    <div className="back-to-top" onClick={scrollToTop}>
+    <button
+      type="button"
+      className="back-to-top"
+      aria-label={I18nUtils.getMessage('accessibility_back_to_top')}
+      onClick={scrollToTop}>
       <SVGIcon icon={SVGIcons.WALLET_HISTORY_TOP} />
-    </div>
+    </button>
   );
 };
